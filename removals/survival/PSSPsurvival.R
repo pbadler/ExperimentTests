9296a992afdefe4df88cc8367318ecb20062d658
peterbadler <peter.adler@usu.edu>
2016-03-24 16:05:09 -0600

    Adding "Others" to survival regressions

--- removals/survival/PSSPsurvival.R
+++ removals/survival/PSSPsurvival.R
@@ -11,77 +11,25 @@ setwd(paste(root,"/ExperimentTests/removals/survival",sep="")); # modify as need
 #########################################
 
 doSpp <- "PSSP"
-sppList <- c("ARTR","HECO","POSE","PSSP")
+sppList <- c("ARTR","HECO","POSE","PSSP","allcov","allpts")
 dataDir1 <- paste(root,"/driversdata/data/idaho",sep="")
 dataDir2 <- paste(root,"/driversdata/data/idaho_modern",sep="")
 nonCompLength.s=5 #Number of columns in SppData that are not measures of competitors 
 
-# import old data--------------------------------------------------------
-survDfile=paste(dataDir1,"/speciesdata/",doSpp,"/survD.csv",sep="")
-survD=read.csv(file=survDfile)
-D1=survD[survD$allEdge==0,];
-D1$year <- D1$year
-D1$logarea=log(D1$area)
-D1$quad=as.character(D1$quad)
-
-# import neighbor data
-ringD <- read.csv(paste(dataDir1,"/speciesdata/",doSpp,"/",doSpp,"_nbhood_rings.csv",sep=""))
-ringD$year<-ringD$year
-
-# merge D with ringD (D contains fewer rows)
-D1<-merge(D1,ringD,by.x=c("quad","year","trackID"),by.y=c("quad","year","genetID"))
-D1=D1[order(D1$X),]
-rm(ringD,survD)
-row.names(D1) <- NULL  
-
-# calculate W's (MAKE SURE NOT TO REORDER D!)
-W <- matrix(NA,NROW(D1),length(sppList))
-colnames(W) <- paste("W.",sppList,sep="")
+# set up distance weights------------------------------------------------
 dists <- read.csv(paste(dataDir1,"/speciesdata/IdahoDistanceWeights.csv",sep=""));
-for(iSpp in 1:length(sppList)){
-  neighborCols=which(substr(names(D1),1,4)==sppList[iSpp]) # pull out annulus data for the focal species 
-  dist_wts<- dists[,paste0(sppList[iSpp])]
-  C <- data.matrix(D1[,neighborCols]) #matrix of conspecific areas in the annuli 
-  W[,iSpp] <- C%*%dist_wts 
-}
+dists$allcov <- rowMeans(dists[,1:4])  # for "other" polygons use average of big 4
+dists$allpts <- dists$POSE  # set forb dist wts = smallest grass (POSE)
 
-# reformat D
-D1 <- D1[,c("X","quad","year","trackID","area","survives","age","distEdgeMin","allEdge","seedling","QuadName","Grazing","Group","logarea","species")]
-D1 <- cbind(D1,W)
+# import old data--------------------------------------------------------
+
+source("fetchSurvData.r")
+
+D1 <- fetchSdat(doSpp=doSpp,speciesList=sppList,datadir=dataDir1,distWts=dists)
 D1$Treatment <- "Control"
 
 # import modern data--------------------------------------------------------
-survDfile=paste(dataDir2,"/speciesdata/",doSpp,"/survD.csv",sep="")
-survD=read.csv(file=survDfile)
-D2=survD[survD$allEdge==0,];
-D2$year <- D2$year
-D2$logarea=log(D2$area)
-D2$quad=as.character(D2$quad)
-
-# import neighbor data
-ringD <- read.csv(paste(dataDir2,"/speciesdata/",doSpp,"/",doSpp,"_nbhood_rings.csv",sep=""))
-ringD$year<-ringD$year
-
-# merge D with ringD (D contains fewer rows)
-D2<-merge(D2,ringD,by.x=c("quad","year","trackID"),by.y=c("quad","year","genetID"))
-D2=D2[order(D2$X),]
-rm(ringD,survD)
-row.names(D2) <- NULL  
-
-# calculate W's (MAKE SURE NOT TO REORDER D!)
-W <- matrix(NA,NROW(D2),length(sppList))
-colnames(W) <- paste("W.",sppList,sep="")
-#dists <- read.csv(paste(dataDir1,"/speciesdata/IdahoDistanceWeights.csv",sep=""));
-for(iSpp in 1:length(sppList)){
-  neighborCols=which(substr(names(D2),1,4)==sppList[iSpp]) # pull out annulus data for the focal species 
-  dist_wts<- dists[,paste0(sppList[iSpp])]
-  C <- data.matrix(D2[,neighborCols]) #matrix of conspecific areas in the annuli 
-  W[,iSpp] <- C%*%dist_wts 
-}
-
-# reformat D
-D2 <- D2[,c("X","quad","year","trackID","area","survives","age","distEdgeMin","allEdge","seedling","QuadName","Grazing","Group","logarea","species")]
-D2 <- cbind(D2,W)
+D2 <- fetchSdat(doSpp=doSpp,speciesList=sppList,datadir=dataDir2,distWts=dists)
 
 # merge in treatment data
 tmp <- read.csv(paste(dataDir2,"/quad_info.csv",sep=""))
@@ -99,7 +47,7 @@ if(doSpp!="ARTR"){
 
 # combine old and modern
 allD <- rbind(D1,D2)
-rm(D1,D2,tmp,W)
+rm(D1,D2,tmp)
 
 # clean up dataset ----------------------------------------------
 allD$year[allD$year<2000] <- allD$year[allD$year<2000] + 1900
