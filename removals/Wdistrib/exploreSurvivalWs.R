a8e485cd012ad52bb35a09ef4c341fad56042165
peterbadler <peter.adler@usu.edu>
2016-04-25 16:32:18 -0600

    Use combined historical and modern distance weights

--- removals/Wdistrib/exploreSurvivalWs.R
+++ removals/Wdistrib/exploreSurvivalWs.R
@@ -15,7 +15,7 @@ dataDir1 <- paste(root,"/driversdata/data/idaho",sep="")
 dataDir2 <- paste(root,"/driversdata/data/idaho_modern",sep="")
 
 # set up distance weights
-dists <- read.csv(paste(dataDir1,"/speciesdata/IdahoDistanceWeights.csv",sep=""));
+dists <- read.csv(paste(dataDir2,"/speciesdata/IdahoModDistanceWeights.csv",sep=""));
 dists$allcov <- rowMeans(dists[,1:4])  # for "other" polygons use average of big 4
 dists$allpts <- dists$POSE  # set forb dist wts = smallest grass (POSE)
 
