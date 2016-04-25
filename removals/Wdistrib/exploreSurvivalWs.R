b7b6ac8807bec0c8cf5d5ffbb1c2ea3521460b4d
peterbadler <peter.adler@usu.edu>
2016-04-25 13:34:44 -0600

    edits to W distribution script

--- removals/Wdistrib/exploreSurvivalWs.R
+++ removals/Wdistrib/exploreSurvivalWs.R
@@ -57,142 +57,10 @@ for(iSpp in 1:4){
 
 names(allD) <- sppList[1:4]
 
-#####################
-#  2. Model W's (with help from Giles Hooker)
-####################
-
-doSpp <- 4 # look at PSSP
-
-# grab control treatment data
-cD <- subset(allD[[doSpp]],Treatment=="Control") 
-groupI <- which(cD$Group=="E1") # keep track of this for later
-Wdat = cD[,4:ncol(cD)]
-Wnonzero = Wdat > 0
-
-#scatter plots
-pairs(Wdat)
-
-# grab data from no shrub plots
-trtD <- subset(allD[[doSpp]],Treatment=="No_shrub" & year==2011)  
-Wdat.trt = trtD[,4:ncol(trtD)]
-Wnonzero.trt = Wdat.trt > 0
-pairs(Wdat.trt)
-
-myCol <- c(rep("black",dim(Wdat)[1]),rep("red",dim(Wdat.trt)[1]))
-allWdat <-rbind(Wdat,Wdat.trt)
-X11()
-pairs(allWdat,col=myCol)
-
-
-
-# Brief analysis -- are there correlations among whether there is a zero or not
-cor(Wnonzero) # All pretty tiny. 
-cor(Wnonzero.trt) # pretty strong negative correlation with HECO
-
-# To analyze the rest, we'll look at Box-Cox transformations - these find lambda 
-# so that X^lambda is as close to normal as possible. boxcox() in R just produces
-# a list of lambdas and the corresponding likelihood so we have to do a bit of
-# processing
-library(MASS)
-W.bc = Wdat  # will store transformed data
-W.bc.trt = Wdat.trt
-for(i in 1:6){
-  
-  # Select non-zero entries for this column
-  t.dat = Wdat[Wnonzero[,i],i]
-  
-  # boxcox
-  bc = boxcox(t.dat~1,lambda = seq(-2,2,by=0.01),plotit=FALSE)
- 
-  # optimal lambda
-  lambda = bc$x[which.max(bc$y)]
-  
-  # Add into W.bc
-  W.bc[Wnonzero[,i],i] = t.dat^lambda
-  
-  # apply same transformation to treatment plots
-  W.bc.trt[Wnonzero.trt[,i],i] = Wdat.trt[Wnonzero.trt[,i],i]^lambda
-  
-}
-
-# compare distributions of control and treatment plot W's
-
-filename <- paste0(sppList[doSpp],"_W_byTrt.pdf")
-pdf(filename,height=5,width=8.5)
-  par(mfrow=c(2,3),tcl=-0.2,mgp=c(2,0.5,0),mar=c(2,2,2,1),oma=c(2,2,0,0))
-  for(i in 1:6){
-    
-    pNonZero <-sum(Wnonzero[groupI,i])/length(Wnonzero[groupI,i]) # just E1 control quads
-    #pNonZero <-sum(Wnonzero[,i])/length(Wnonzero[,i])  # all control quads
-    pNonZero.trt <- sum(Wnonzero.trt[,i])/length(Wnonzero.trt[,i]) 
-    dens <- density(W.bc[which(Wnonzero[,i]==1 & cD$Group=="E1"),i]) # just E1 control quads
-    #dens <- density(W.bc[Wnonzero[,i],i])  # all control quads
-    dens.trt <- density(W.bc.trt[Wnonzero.trt[,i],i])
-    ylims <- c(0,1.2*max(c(dens$y,dens.trt$y)))
-    xlims <- c(min(c(dens$x,dens.trt$x)),max(c(dens$x,dens.trt$x)))
-    plot(dens,xlab="",ylab="",,xlim=xlims,ylim=ylims,main=names(cD)[3+i],col="black")
-    lines(dens.trt,col="red")
-    legend("topleft",c(paste0("Control, p(W>0)=",round(pNonZero,2)),
-                       paste0("No shrub, p(W>0)=",round(pNonZero.trt,2))),
-           lty=1,col=c("black","red"),bty="n")
-    
-  }
-  
-  mtext("Box-Cox W",side=1,line=0.5,outer=T)
-  mtext("Density",side=2,line=0.5,outer=T)
-
-dev.off()
-
-# A first analysis: correlation based on rows where there are no zeros
-W.all = apply(Wnonzero,1,prod)
-cor(W.bc[W.all==1,])
-# Just a few correlations larger than 0.1, none over 0.15
-
-# do it again with just PSSP and ARTR
-W.all = apply(Wnonzero[,c(1,4)],1,prod)
-cor(W.bc[W.all==1,c(1,4)])
-# almost 0.19
-
-# Further analyses are based on conditionals; here we'll regress each column on 
-# the others, allowing a separate effect for being zero in each. We'll also 
-# predict whether or not the response is non-zero via a logistic regression. 
-
-lm.mods = list()       # Models for non-zeros
-glm.mods = list()      # Models to predict whether or not you're zero
-Rsq = rep(0,4)         # R-squared for non-zero model
-ave.dev = rep(0,4)     # Average drop in deviance per observation
-
-for(i in 1:6){
- # Linear regression for non-zeros
- t.dat = data.frame(y = W.bc[Wnonzero[,i],i],W.bc[Wnonzero[,i],-i], Wnonzero[Wnonzero[,i],-i])
- 
- lm.mods[[i]] = lm(y~.,data=t.dat)
-
- print(summary(lm.mods[[i]]))
- Rsq[i] = summary(lm.mods[[i]])$r.squared
-
- # Generalized Linear regression for whether or not the record is zero
- t.dat = data.frame(y = Wnonzero[,i],W.bc[,-i], Wnonzero[,-i])
- glm.mods[[i]] = glm(y~.,data=t.dat,family='binomial')
-
- print(summary(glm.mods[[i]])) 
- 
- ave.dev[i] = (glm.mods[[i]]$null.deviance - glm.mods[[i]]$deviance)/nrow(t.dat)
-}
-
-# Some of these effects are significant, but the change in explanatory power is 
-# tiny
-
-
-#####################
-#  3. Visualize W's
-####################
-
-Wcols <- grep("W.*",names(allD[[1]]))
-
 # histograms
+Wcols <- grep("W.*",names(allD[[1]]))
 pdf("W-histograms.pdf",height=10,width=4)
-par(mfrow=c(6,2),mgp=c(2,0.5,0),tcl=-0.2,oma=c(0,0,2,0))
+par(mfrow=c(6,2),mgp=c(2,0.5,0),tcl=-0.2,mar=c(3,3,1,1),oma=c(0,0,2,0))
 for(i in 1:4){
   for(j in 1:length(sppList)){
     hist(allD[[i]][,Wcols[j]],breaks=20,xlab="W",main=names(allD[[i]])[Wcols[j]])
@@ -202,12 +70,150 @@ for(i in 1:4){
 }
 dev.off()
 
-# scatter plot matrix
-pdf("W-scatter.pdf",height=6,width=6)
-for(i in 1:4){
-  pairs(sqrt(allD[[i]][,Wcols]),main=sppList[i])
-}
-dev.off()
+#####################
+#  2. Model W's (with help from Giles Hooker)
+####################
+
+# store results of regressions
+nonzero.rsq <- matrix(NA,4,6)
+row.names(nonzero.rsq) <- sppList
+zero.dev <-matrix(NA,4,6)
+row.names(zero.dev) <- sppList
+
+for(doSpp in 1:4){
+  
+  # grab control treatment data
+  cD <- subset(allD[[doSpp]],Treatment=="Control") 
+  groupI <- which(cD$Group=="E1") # keep track of this for later
+  Wdat = cD[,4:ncol(cD)]
+  Wnonzero = Wdat > 0
+  
+  # grab data from no shrub plots
+  trtD <- subset(allD[[doSpp]],Treatment=="No_shrub" & year==2011)  
+  Wdat.trt = trtD[,4:ncol(trtD)]
+  Wnonzero.trt = Wdat.trt > 0
+  
+  # scatter plots
+  filename <- paste0(sppList[doSpp],"_W_scatters.pdf")
+  tmp <- rgb(0,100,255,alpha=125,maxColorValue = 255)
+  myCol <- c(rep(tmp,dim(Wdat)[1]),rep("red3",dim(Wdat.trt)[1]))
+  pdf(filename,height=8,width=8)
+    par(tcl=-0.2,mgp=c(2,0.5,0))
+    allWdat <-rbind(Wdat,Wdat.trt)
+    pairs(allWdat,col=myCol)
+  dev.off()
+  
+  # compare distributions of control and treatment plot W's
+  filename <- paste0(sppList[doSpp],"_W_byTrt.pdf")
+  pdf(filename,height=5,width=8.5)
+    par(mfrow=c(2,3),tcl=-0.2,mgp=c(2,0.5,0),mar=c(2,2,2,1),oma=c(2,2,0,0))
+    for(i in 1:6){
+      
+      dens <- density(log(Wdat[Wnonzero[groupI,i],i])) # just E1 control quads
+      #dens <- density(W.bc[Wnonzero[,i],i])  # all control quads
+      dens.trt <- density(log(Wdat.trt[Wnonzero.trt[,i],i]))
+      
+      ylims <- c(0,1.2*max(c(dens$y,dens.trt$y)))
+      xlims <- c(min(c(dens$x,dens.trt$x)),max(c(dens$x,dens.trt$x)))
+      plot(dens,xlab="",ylab="",xlim=xlims,ylim=ylims,main=names(cD)[3+i],col="dodgerblue3")
+      lines(dens.trt,col="red3")
+      
+      if(i==6){
+        mtext("log(W)",side=1,line=0.5,outer=T)
+        mtext("Density",side=2,line=0.5,outer=T)
+      }
+      
+      # add inset
+      pNonZero <-sum(Wnonzero[groupI,i])/length(Wnonzero[groupI,i]) # just E1 control quads
+      #pNonZero <-sum(Wnonzero[,i])/length(Wnonzero[,i])  # all control quads
+      pNonZero.trt <- sum(Wnonzero.trt[,i])/length(Wnonzero.trt[,i]) 
+      subplot(barplot(c(pNonZero,pNonZero.trt),names=c("Con","Rem"),ylim=c(0,1),
+              ylab="p(W>0)",xlab="",col=c("dodgerblue3","red3")),
+              x=c(xlims[1]+(xlims[2]-xlims[1])/6,xlims[1]+(xlims[2]-xlims[1])/2),
+              y=c(ylims[2]-(ylims[2]-ylims[1])/5,ylims[2]-(ylims[2]-ylims[1])/20),
+              par=list(tcl=-0.2,mgp=c(2,0.5,0),mar=c(2,3,0,0)))
+      
+    } 
+  dev.off()
+  
+  # Brief analysis -- are there correlations among whether there is a zero or not
+  cor(Wnonzero) # All pretty tiny. 
+  cor(Wnonzero.trt) # pretty strong negative correlation with HECO
+  
+  # To analyze the rest, we'll look at Box-Cox transformations - these find lambda 
+  # so that X^lambda is as close to normal as possible. boxcox() in R just produces
+  # a list of lambdas and the corresponding likelihood so we have to do a bit of
+  # processing
+  library(MASS)
+  W.bc = Wdat  # will store transformed data
+  W.bc.trt = Wdat.trt
+  for(i in 1:6){
+    
+    # Select non-zero entries for this column
+    t.dat = Wdat[Wnonzero[,i],i]
+    
+    # boxcox
+    bc = boxcox(t.dat~1,lambda = seq(-2,2,by=0.01),plotit=FALSE)
+   
+    # optimal lambda
+    lambda = bc$x[which.max(bc$y)]
+    
+    # Add into W.bc
+    W.bc[Wnonzero[,i],i] = t.dat^lambda
+    
+    # apply same transformation to treatment plots
+    W.bc.trt[Wnonzero.trt[,i],i] = Wdat.trt[Wnonzero.trt[,i],i]^lambda
+    
+  }
+  
+  # A first analysis: correlation based on rows where there are no zeros
+  W.all = apply(Wnonzero,1,prod)
+  cor(W.bc[W.all==1,])
+  # Just a few correlations larger than 0.1, none over 0.15
+  
+  
+  # Further analyses are based on conditionals; here we'll regress each column on 
+  # the others, allowing a separate effect for being zero in each. We'll also 
+  # predict whether or not the response is non-zero via a logistic regression. 
+  
+  lm.mods = list()       # Models for non-zeros
+  glm.mods = list()      # Models to predict whether or not you're zero
+  Rsq = rep(0,4)         # R-squared for non-zero model
+  ave.dev = rep(0,4)     # Average drop in deviance per observation
+  
+  for(i in 1:6){
+   # Linear regression for non-zeros
+   t.dat = data.frame(y = W.bc[Wnonzero[,i],i],W.bc[Wnonzero[,i],-i], Wnonzero[Wnonzero[,i],-i])
+   
+   lm.mods[[i]] = lm(y~.,data=t.dat)
+  
+   #print(summary(lm.mods[[i]]))
+   Rsq[i] = summary(lm.mods[[i]])$r.squared
+  
+   # Generalized Linear regression for whether or not the record is zero
+   t.dat = data.frame(y = Wnonzero[,i],W.bc[,-i], Wnonzero[,-i])
+   glm.mods[[i]] = glm(y~.,data=t.dat,family='binomial')
+  
+   #print(summary(glm.mods[[i]])) 
+   
+   ave.dev[i] = (glm.mods[[i]]$null.deviance - glm.mods[[i]]$deviance)/nrow(t.dat)
+  }
+  # Some of these effects are significant, but the change in explanatory power is 
+  # tiny
+
+  nonzero.rsq[doSpp,] <-Rsq
+  zero.dev[doSpp,] <- ave.dev
+
+} # next doSpp
+
+print(nonzero.rsq)
+print(max(nonzero.rsq))
+print(zero.dev)
+print(max(zero.dev))
+
+#####################
+#  3. Visualize W's
+####################
 
 # wind rose (four dimensions)
 # myRoot <- 3
