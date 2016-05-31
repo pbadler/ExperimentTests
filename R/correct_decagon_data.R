a63a7465cafb183db14de45f29d181fffa9a4001
Andy Kleinhesselink <andy.kleinhesselink@gmail.com>
2016-05-31 09:37:57 -0600

    adding csv and R updates

--- R/correct_decagon_data.R
+++ R/correct_decagon_data.R
@@ -16,7 +16,33 @@ length( df$date[df$plot == 11 & df$date_started == '2014-04-13']  )
 
 length( df$date[df$plot == '11_12_C' & df$date_started == '2014-04-13']  ) 
 
+times <- df %>% select( PrecipGroup, period, plot, date ) %>% distinct( . )
 
+times <- times  %>% arrange( PrecipGroup, period, plot, date ) %>% mutate ( )
+
+head(times )
+
+times$old_time <- times$date 
+
+times 
+
+head( times %>% group_by(PrecipGroup, period) %>% spread( plot , date ) ) 
+
+times_list <- split(times, factor( paste0(times$PrecipGroup, times$period )) )
+
+head( times_list[[4]] ) 
+
+
+head( times_list[[4]] %>% ungroup(.) %>% spread(plot, date ) ) 
+
+
+df <- expand.grid ( x = 1:10, y = 1:100)
+
+df %>% spread( x, y )
+
+head(times)
+unique(times$id)
+?spread
 
 
 
@@ -26,3 +52,15 @@ length( df$date[df$plot == '11_12_C' & df$date_started == '2014-04-13']  )
 
 
 # Correct switched ports -------------------------------------------------------------------------
+
+stocks <- data.frame(
+  time = as.Date('2009-01-01') + 0:9,
+  X = rnorm(10, 0, 1),
+  Y = rnorm(10, 0, 2),
+  Z = rnorm(10, 0, 4)
+)
+
+stocksm <- stocks %>% gather(stock, price, -time)
+head( stocksm ) 
+stocksm %>% spread(stock, price)
+stocksm %>% spread(time, price)
