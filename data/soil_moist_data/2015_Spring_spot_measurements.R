22113d828804b15e9966073e80b4fa66252b4e17
Andy Kleinhesselink <andy.kleinhesselink@gmail.com>
2016-06-22 19:09:27 -0600

    adding spring spot measurements and analysis script

--- data/soil_moist_data/2015_Spring_spot_measurements.R
+++ data/soil_moist_data/2015_Spring_spot_measurements.R
@@ -2,13 +2,46 @@
 ##### 
 
 rm(list = ls())
-library (ggplot2 )
-library( reshape )
+library(ggplot2 )
+library(dplyr)
+library(tidyr)
 
-plots = read.csv('../precip_plots.csv')
-plots = plots[ , c('plot_id', 'block', 'treatment') ]
+q_info <- read.csv('data/quad_info.csv')
 
-readings = read.csv('2015_spring/2015-04-29-precip_experiment_soil_moisture.csv', skip = 2)
+
+calibration <- read.csv('data/soil_moist_data/spot_measurements/2015-05-07_soil_probe_calibration.csv')
+
+
+p1 <- read.csv('data/soil_moist_data/spot_measurements/2012-06-06_spot_measurements.csv', skip = 3)
+
+p1$date <- '2012-06-06'
+p1$Plot <- gsub( p1$Plot, pattern = '-', replacement = '_')
+p1$rep <- c(1:2)
+p1 <- p1 %>% rename( plot = Plot )
+
+p2 <- read.csv('data/soil_moist_data/spot_measurements/2015-05-07_spot_measurements.csv') 
+p3 <- read.csv('data/soil_moist_data/spot_measurements/2016-05-10_spot_measurements.csv')
+p4 <- read.csv('data/soil_moist_data/spot_measurements/2015-06-09_spot_measurements.csv')
+
+df <- rbind( p2, p3, p4)
+
+df <- df %>% gather( key = rep, PCT, E1:W3 )
+
+df <- rbind( p1, df )
+
+# run calibration ---------------------------------------------------------------------------------------------------- 
+
+calibration %>% 
+  mutate( wet_weight = (wet_weight- bag_weight) , dry_weight = (dry_weight - bag_weight), 
+          water = wet_weight - dry_weight, soil_moisture_g = 100*water/dry_weight) 
+
+
+readings$plot_id <- readings$plot
+
+readings
+merge( readings, plots, by= 'plot_id' ) 
+
+
 readings_long = melt(readings, id.var = 'plot')
 moisture = merge( plots, readings_long, by.x = 'plot_id', by.y = 'plot' , all.y = TRUE)
 levels(moisture$treatment ) <- c(levels(moisture$treatment), 'control')
