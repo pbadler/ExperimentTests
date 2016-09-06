1dc9024c55597f749756671aa33a9875c9419eac
Andy Kleinhesselink <andy.kleinhesselink@gmail.com>
2016-09-06 16:01:54 -0600

    adding some changes to the make climate variables script and the script that makes plots of all the variables across the historical and contemporary period

--- R/make_climate_variables.R
+++ R/make_climate_variables.R
@@ -12,39 +12,57 @@ library(dplyr)
 library(lme4)
 library(zoo)
 
-station_dat <- read.csv('data/USSES_climate.csv')
-station_dat <- read.csv('data/USSES_climate_monthly.csv')
 
+dm <- c(4, 11 ) # drought months 
+im <- c(5, 10 ) # irrigation months
+
+# make time periods --------------------------------------------------------------------
+
+p1 <- data.frame( period = 'contemporary', year = 2007:2016)
+p2 <- data.frame( period = 'not monitored', year = 1956:2006)
+p3 <- data.frame( period = 'historical', year = 1926:1955)
+periods <- data.frame( rbind( p1, p2, p3 )) 
+
+# ----- read in data --------------------------------------------------------------------#
+
+station_dat <- read.csv('data/USSES_climate_monthly.csv')
 season <- read.csv('data/season_table.csv')
 
-# ---------------------------------------------------------------------------------------
+# ---process dates----------------------------------------------------------------------#
+
 station_dat$date <-  as.POSIXct( strptime( station_dat$DATE, '%Y%m%d', tz = 'MST')  ) 
-#station_dat$doy <- as.numeric(strftime( station_dat$date, '%j'))
 station_dat$month <- as.numeric(strftime( station_dat$date, '%m'))
 station_dat$year <- as.numeric(strftime( station_dat$date, '%Y'))
+
+# -------- NAs -------------------------------------------------------------------------#
 
 station_dat$TPCP[ station_dat$TPCP == -9999.0 ] <- NA
 station_dat$MMNT[ station_dat$MMNT == -9999.0 ] <- NA
 station_dat$MMXT[ station_dat$MMXT == -9999.0 ] <- NA
 station_dat$MNTM[ station_dat$MNTM == -9999.0 ] <- NA
 
-# set-up aggregate seasonal variables for model ----------------------------------------- 
+# set-up aggregate seasonal variables for model ----------------------------------------#
 
 df <- merge( station_dat, season, by = 'month')
 
 df <- 
   df %>% 
   mutate( water_year = year + lag_year ) %>% 
-  select(year, month, year, season, season_label, precip_seasons, water_year, TPCP, MNTM, MMXT, MMNT)
+  mutate( quarter = cut(month, 4, labels = paste0('Q', 1:4))) %>%
+  select(year, quarter, month, year, season, season_label, precip_seasons, water_year, TPCP, MNTM, MMXT, MMNT)
 
+# ---------- annual average Temperature -------------------------------------------------#
 annual_MAT <- 
   df %>% 
   group_by( year ) %>%
   summarise (MAT = mean(MNTM, na.rm = TRUE))
 
+# ---------- annual total precip --------------------------------------------------------#
 annual_TPPT <- 
   df %>% group_by( year ) %>% 
   summarise( TPPT = mean(TPCP, na.rm = TRUE), n = n())
+
+# ---------- seasonal average Temperature -----------------------------------------------#
 
 seasonal_tmean <- 
   df %>% 
@@ -64,26 +82,47 @@ seasonal_tmean <-
   unite( stat,  season_label, lag , sep = '_TMEAN_') %>% 
   spread( stat, MNTM )
 
-monthly_precip <- 
+# ---------- monthly climate  -----------------------------------------------------------#
+# 
+# incorporate drought and irrigation effects only in specificied months 
+# 
+
+monthly_clim <- 
   df %>% 
-  group_by(year, water_year, precip_seasons, month) %>% 
-  summarise(control = sum(TPCP, na.rm = TRUE)) %>% 
-  mutate( drought    = ifelse( month %in% c(3:11), control*0.5, control) ) %>% 
-  mutate( irrigation = ifelse( month %in% c(4:10), control*1.5, control) ) %>% 
+  mutate(control = TPCP) %>% 
+  select(-TPCP) %>% 
+  mutate( drought    = ifelse( month %in% c(dm[1]:dm[2]), control*0.5, control) ) %>% 
+  mutate( irrigation = ifelse( month %in% c(im[1]:im[2]), control*1.5, control) ) %>% 
   gather(treatment, TPCP, control, drought, irrigation)
-  
+
+# ------------ aggregate monthly climate with treatment effects by quarter ---------------#
+
+quarterly_climate <-
+  monthly_clim %>% 
+  gather( var, val ,  MMNT, MMXT, MNTM, TPCP ) %>% 
+  group_by( treatment, var, year, quarter ) %>% 
+  summarise( avg = mean(val), ttl = sum(val) ) %>% 
+  group_by(var) %>% 
+  gather( stat, val, avg, ttl ) %>% 
+  filter( !(var == 'TPCP' & stat == 'avg' )) %>% 
+  group_by( treatment, var, stat) %>% 
+  arrange(year, quarter) %>%
+  ungroup() %>% 
+  unite(var, c(var, stat) , sep = '_')
+
+# -------------------------------------------------------------------------------------------#
+# -------------- aggregate monthly climate with treatment effects by season -----------------#
+
 seasonal_precip <- 
-  df %>% 
-  group_by(year, water_year, precip_seasons, month) %>% 
-  summarise(Control = sum(TPCP, na.rm = TRUE)) %>% 
-  mutate( Drought    = ifelse( month %in% c(3:11), Control*0.5, Control) ) %>% 
-  mutate( Irrigation = ifelse( month %in% c(4:10), Control*1.5, Control) ) %>% 
-  gather(Treatment, TPCP, Control, Drought, Irrigation) %>%
-  group_by(water_year, precip_seasons , Treatment ) %>% 
-  summarise(l0  = sum(TPCP, na.rm = TRUE)) %>% 
+  monthly_clim %>% 
+  select ( -MMNT, -MMXT, -MNTM) %>%  
+  group_by( treatment, water_year, precip_seasons ) %>% 
+  summarise( TPCP = sum(TPCP) ) %>% 
+  group_by( treatment, precip_seasons ) %>% 
+  arrange( water_year, precip_seasons) %>%
   rename( year = water_year ) %>% 
-  group_by( Treatment, precip_seasons ) %>% 
   arrange( year ) %>% 
+  rename( l0 = TPCP) %>% 
   mutate( l1 = lag (l0, 1 ) , 
           l2 = lag (l0, 2 ) ) %>% 
   gather( lag, TPCP, l0:l2) %>% 
@@ -91,21 +130,23 @@ seasonal_precip <-
   unite( stat , precip_seasons, lag , sep = '_PRCP_') %>% 
   spread( stat, TPCP )
 
+# -------------- join dfs for variables -------------------------------------------------------#
+
 seasonal_clim <- left_join( seasonal_tmean, seasonal_precip, by = 'year') 
 
 annual_clim <- left_join( annual_TPPT, annual_MAT)
 
 seasonal_clim <- left_join( annual_clim, seasonal_clim)
 
-# join periods
+# -------join periods -------------------------------------------------------------------------#
+seasonal_clim <- left_join( seasonal_clim, periods )
 
-p1 <- data.frame( period = 'contemporary', year = 2007:2016)
-p2 <- data.frame( period = 'not monitored', year = 1956:2006)
-p3 <- data.frame( period = 'historical', year = 1926:1955)
+# ------ filter out treatments ----------------------------------------------------------------# 
+seasonal_clim <- seasonal_clim %>% 
+  filter( !(year < 2012 & treatment != 'control')) # remove all drought and irrigation treatments prior to 2012 
 
-seasonal_clim <- left_join( seasonal_clim, data.frame( rbind(p1, p2, p3)) )
-
-seasonal_clim <- seasonal_clim %>% filter( !(year < 2012 & Treatment != 'Control'))
+# -------- output -----------------------------------------------------------------------------#
 
 saveRDS( seasonal_clim, 'data/temp_data/seasonal_climate.RDS')
 saveRDS( df, 'data/temp_data/monthly_climate.RDS')
+saveRDS( quarterly_climate, 'data/temp_data/quarterly_climate.RDS')
--- R/plot_climate_by_period_and_treatment.R
+++ R/plot_climate_by_period_and_treatment.R
@@ -13,14 +13,12 @@ library(ggplot2)
 library(stringr)
 
 climate <- readRDS('data/temp_data/seasonal_climate.RDS')
-# monthly <- readRDS('data/temp_data/monthly_climate.RDS')
-# 
-# monthly <- monthly %>% arrange(year, month) 
 
 #------------------------------------------------------------------------ 
 
 colors <- c('#fc8d62', 'gray', '#8da0cb')
 climate$period[ climate$year == 1925 ] <- 'not monitored'
+
 
 climate_long <- 
   climate %>% 
@@ -29,14 +27,14 @@ climate_long <-
   rename( `Mean Annual Temperature` = MAT) %>% 
   rename( `Cool Season PPT` = cool_PRCP_l0, `Warm Season PPT` = warm_PRCP_l0) %>% 
   mutate( `Total Annual PPT` = `Cool Season PPT` + `Warm Season PPT`) %>%
-  select( year, period, Treatment,  `Mean Annual Temperature`, `Total Annual PPT`, `Cool Season PPT`, `Warm Season PPT`, `Spring Temperature`, `Summer Temperature`, `Fall Temperature`, `Winter Temperature`) %>% 
+  select( year, period, treatment,  `Mean Annual Temperature`, `Total Annual PPT`, `Cool Season PPT`, `Warm Season PPT`, `Spring Temperature`, `Summer Temperature`, `Fall Temperature`, `Winter Temperature`) %>% 
   gather( season, value , `Mean Annual Temperature`:`Winter Temperature`) %>% 
   mutate( ylabel = ifelse( str_detect(season, pattern = 'PPT'), 'Total~precipitation~(mm)', 'Average ~ air~temperature~degree*C')) 
 
 
 annual_plots <- 
   climate_long %>% 
-  #filter(Treatment == 'Control') %>%
+  #filter(treatment == 'control') %>%
   group_by(period) %>% 
   mutate( label_x = median(year)) %>%
   mutate(period_label = paste0(period, '\n', min(year), '-', max(year))) %>%
@@ -59,8 +57,8 @@ base_ts <- function( x ) {
   
   if(str_detect(x$season[1], "PPT")){ 
     g +   
-      geom_point(aes(shape = Treatment, color = Treatment)) + 
-      geom_line(aes( group = Treatment, linetype = Treatment, color = Treatment))  
+      geom_point(aes(shape = treatment, color = treatment)) + 
+      geom_line(aes( group = treatment, linetype = treatment, color = treatment))  
   }else{ 
     g + 
       geom_point() + 
@@ -69,7 +67,6 @@ base_ts <- function( x ) {
 }
 
 ts_plots <- annual_plots %>% group_by( season ) %>% do( p = base_ts(.) )
-
 
 pdf('figures/annual_climate_comparison_timeseries.pdf', height = 8 , width = 10)
 print( ts_plots$p ) 
@@ -80,21 +77,20 @@ dev.off()
 rank_df <- 
   annual_plots %>% 
   group_by(season) %>% 
-  filter( !( !str_detect(pattern = 'PPT', season) & Treatment != 'Control') ) %>% 
+  filter( !( !str_detect(pattern = 'PPT', season) & treatment != 'control') ) %>% 
   mutate( rank_order = rank(`value`, ties.method = 'first')) %>% 
   mutate( xlab = ifelse( str_detect(pattern = "PPT", season ), 'Year rank from driest to wettest', 'Year rank from coldest to hotest'))
-
 
 rank_plots <- function( x ) { 
 
   ggplot( data = x, aes( x = rank_order, y = value, fill = period)) + 
       geom_bar(stat = 'identity') + 
-      geom_point( data = subset(x , Treatment != 'Control'), aes( y = 1.05*value, shape = Treatment, color = NULL), size = 6) + 
+      geom_point( data = subset(x , treatment != 'control'), aes( y = 1.05*value, shape = treatment, color = NULL), size = 6) + 
       ylab(label =  parse( text = x$ylabel[1])) +
       xlab(label =  x$xlab[1]) + 
       scale_shape_manual(values = c('-', '+')) + 
       scale_fill_manual(values = colors) +  
-      guides(fill = guide_legend('Period', override.aes = list(size = 0, color = NA)), colour = 'none', shape = guide_legend('Treatment') )  + 
+      guides(fill = guide_legend('Period', override.aes = list(size = 0, color = NA)), colour = 'none', shape = guide_legend('treatment') )  + 
     ggtitle( label = x$season[1]) 
     
 }
@@ -108,57 +104,3 @@ pdf('figures/annual_climate_comparison_rank.pdf', height = 8 , width = 10)
 print( rank_gg$p ) 
 dev.off()
 
-# export for peter ------------------------------------------------------------------------- 
-
-climate_out <- climate_long %>% filter( year > 1925 , Treatment == 'Control') %>% rename( variable = season ) 
-
-summary_out <- climate_out %>% group_by( variable, period ) %>% summarise( avg = mean(value), start_year = min(year), end_year = max(year))
-
-summary_out
-
-
-# ---------------------------------------------------------------------------------------
-
-MAT <- ts( ( climate %>% filter( Treatment == 'Control') %>% arrange( year) %>% distinct(year) %>% filter(year < 2016))$MAT )
-
-acf(MAT)
-pacf( MAT)
-
-arima(MAT, order = c(0, 0, 0))
-arima(MAT, order = c(1, 0, 0))
-arima(MAT, order = c(1, 0, 1))
-arima(MAT, order = c(2, 0, 0))
-arima(MAT, order = c(3, 0, 0))
-
-arima(MAT, order = c(0, 0, 1))
-arima(MAT, order = c(0, 0, 2))
-arima(MAT, order = c(1, 0, 3))
-?arima
-m <- arima(MAT, order = c(1,0,1))
-plot( m$residuals)
-plot( MAT, type = 'l')
-
-t <- 1:length(MAT)
-summary(lm(MAT ~ t ))
-summary(lm( m$residuals ~ t ))
-
-plot ( MAT, type = 'l', x = 1924 + t)
-
-# 
-
-MAP <- ts( ( climate %>% arrange( year) %>% distinct(year) %>% filter(year < 2016))$TPPT )
-
-acf(MAP)
-pacf( MAP)
-
-arima(MAP, order = c(1, 0, 1))
-arima(MAP, order = c(1, 0, 0))
-arima(MAP, order = c(0, 0, 1))
-arima(MAP, order = c(0, 0, 0))
-m<- arima(MAP, order = c(1, 0, 0))
-
-plot(m$residuals)
-t <- 1:length(MAP)
-
-summary(lm(residuals(m) ~ t ))
-summary(lm(MAP ~ t))
