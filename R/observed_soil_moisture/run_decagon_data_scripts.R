d02546f5870207914e52626e822fded6c2ef6114
Andy Kleinhesselink <andy.kleinhesselink@gmail.com>
2016-09-09 15:19:24 -0600

    modifying scripts so they refer to the new file paths

--- R/observed_soil_moisture/run_decagon_data_scripts.R
+++ R/observed_soil_moisture/run_decagon_data_scripts.R
@@ -4,12 +4,12 @@
 # 
 ###############################################################################
 
-source('R/import_and_format_decagon_data.R')
+source('R/observed_soil_moisture/import_and_format_decagon_data.R')
 
-source('R/correct_decagon_dates.R')
+source('R/observed_soil_moisture/correct_decagon_dates.R')
 
-source('R/correct_decagon_readings.R')
+source('R/observed_soil_moisture/correct_decagon_readings.R')
 
-source('R/merge_decagon_with_climate_station_data.R')
+source('R/observed_soil_moisture/merge_decagon_with_climate_station_data.R')
 
-source('R/export_daily_soil_moisture_for_SOILWAT.R')
+source('R/observed_soil_moisture/export_daily_soil_moisture_for_SOILWAT.R')
