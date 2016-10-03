d1ac4f2895c9e0d6d7dd3fd4c3a2466bc2cdae07
Andy Kleinhesselink <andy.kleinhesselink@gmail.com>
2016-10-02 22:40:40 -0600

    adding recruitment data manipulation scripts

--- precip/R/prepare_datalists_for_STAN.R
+++ precip/R/prepare_datalists_for_STAN.R
@@ -123,6 +123,88 @@ survival_dataframe2datalist <- function(df, train, hold, clim_vars){
   )
 }
 
+
+
+recruitment_dataframe2datalist <- function(df, train, hold, clim_vars){
+  
+  # # set up data objects for bugs  
+  # y=as.matrix(D[,c(paste("R.",sppList,sep=""))])
+  # R.tot=rowSums(y)
+  # parents1=as.matrix(D[,c(paste("cov.",sppList,sep=""))])/100
+  # parents2=as.matrix(D[,c(paste("Gcov.",sppList,sep=""))])/100
+  # year=as.numeric(as.factor(D$year))
+  # Nyrs=length(unique(D$year))
+  # N=dim(D)[1]
+  # Nspp=length(sppList)
+  # Group=as.numeric(D$Group)
+  # Ngroups=length(unique(Group))
+  # # code treatment specific intercepts: 1 = old and modern control, 2 = no shrub, 3 = no grass
+  # TreatCode=matrix(1,length(y),Nspp)
+  # tmp=which(D$Treatment=="No_shrub")
+  # TreatCode[tmp,2:4]=2
+  # tmp=which(D$Treatment=="No_grass")
+  # TreatCode[tmp,1]=3
+  # test=data.frame(D$year,D$Treatment,TreatCode)
+  
+  
+  # Function simply makes list of data for STAN models
+  
+  # --------split into training and holding data and scale climate covariates -------------------------
+  
+  out <- scale_clim_covs(df, train, hold, clim_vars)
+  
+  training_df <- out[[1]]
+  holding_df <- out[[2]]
+  
+  # --------training data -----------------------------------------------------------------------------
+  N         <- nrow(training_df)                                  # number of data points for training data
+  
+  nyrs      <- length(unique(training_df$year))                   # number of years
+  yid       <- as.numeric(as.factor(training_df$year))            # integer id for each year
+  Y         <- training_df$Y                                      # new recruit at time t + 1   
+  
+  C         <- as.matrix(training_df[, clim_vars])                # all climate covariates
+  Covs      <- ncol(C)                                            # number of climate covariates
+  
+  parents1  <- as.matrix(training_df[, grep('^cov', names(training_df)) ])/100  # parents in plot 
+  parents2  <- as.matrix(training_df[, grep('^Gcov', names(training_df))])/100  # parents in group
+  
+  spp_list  <- factor( str_extract( colnames(parents1), '[A-Z]+$'))        # get species names 
+
+  Nspp      <- length(spp_list)                                   # number of parent species
+  
+  species_name   <- unique(training_df$species )
+  
+  spp       <- as.numeric( spp_list [spp_list == species_name ] ) # assign species number to datalist 
+   
+  gid       <- as.numeric(training_df$Group)                      # integer id for each plot area
+  G         <- length(unique(training_df$Group))                  # number of groups representing exclosure areas
+  
+  #---------hold out/prediction data ------------------------------------------------------------------
+  npreds    <- nrow(holding_df)                                   # total predicted observations, modern data
+  y_holdout <- holding_df$Y                                       # new recruit at time t + 1, modern data
+  
+  Chold     <- holding_df[ , clim_vars]                           # climate matrix, modern data
+  
+  parents1_out  <- as.matrix(holding_df[, grep('^cov',  names(holding_df)) ])/100  # parents in plot 
+  parents2_out  <- as.matrix(holding_df[, grep('^Gcov', names(holding_df))])/100  # parents in group
+  
+  gid_out   <- as.numeric(holding_df$Group)                       # group id, modern data
+  yid_out   <- as.numeric(as.factor(holding_df$year))             # year id, modern data
+  nyrs_out  <- length(unique(holding_df$year))                    # num years, modern data
+  treat_out <- as.numeric(factor(holding_df$Treatment))           # treatments
+  
+  return(
+    list(
+      N = N, Y = Y, Nspp = Nspp, spp = spp, gid = gid, G = G, Yrs = nyrs, yid = yid , Covs = Covs, C = C, parents1 = parents1, parents2 = parents2,
+      gid_out = gid_out, npreds = npreds, y_holdout = y_holdout, Chold = Chold, parents1_out = parents1_out, parents2_out = parents2_out, 
+      yid_out = yid_out, nyrs_out = nyrs_out, treat_out = treat_out,
+      tau_beta = 1
+    )
+  )
+}
+
+
 make_stan_datalist <- function(vr, data_path, clim_vars, clim_file, ... ) { 
 
 
@@ -147,14 +229,14 @@ make_stan_datalist <- function(vr, data_path, clim_vars, clim_file, ... ) {
   
   training <- lapply( all_data, function(x) { which(x$Period == "Historical" & x$Treatment == 'Control') } ) 
   holding  <- lapply( all_data, function(x) { which(x$Period == "Modern" )  } ) 
-  
+
   # -- prepare for stan ---------------------------------------------------------------------# 
   fxn_list <- c('growth_dataframe2datalist', 'survival_dataframe2datalist', 'recruitment_dataframe2datalist')
   
   all_data_list <- mapply( FUN = match.fun( fxn_list [ grep(vr, fxn_list) ]), df = all_data, train = training, hold = holding, MoreArgs = list( 'clim_vars' = clim_vars ), SIMPLIFY = FALSE)
   
   names(all_data_list) <- spp_names
-
+  
   # ---- output ------------------------------------------------------------------------------# 
   
   saveRDS(all_data_list, file.path( 'data/temp_data/', paste0( vr, '_data_lists_for_stan.RDS')))
@@ -172,3 +254,4 @@ data_path <- 'data/temp_data'
 make_stan_datalist('growth', data_path, clim_vars, clim_file )
 make_stan_datalist('survival', data_path, clim_vars, clim_file )
 
+make_stan_datalist('recruitment', data_path, clim_vars, clim_file )
