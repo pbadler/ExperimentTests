140df315c700537b385cf08002696f7d8ca8baac
Andy Kleinhesselink <andy.kleinhesselink@gmail.com>
2016-10-07 18:09:33 -0600

    modifying the prepare datalists script to generate interaction effects and to scale all the competition and interaction effects

--- precip/R/prepare_datalists_for_STAN.R
+++ precip/R/prepare_datalists_for_STAN.R
@@ -12,16 +12,18 @@ library(ggplot2)
 library(zoo)
 library(stringr)
 
-scale_clim_covs <- function(df , train, hold, clim_vars ){ 
+scale_covs <- function(df , train, hold){ 
+  
+  covars <- grep( '^[PTW]\\.', names(df)) # grab all the covariates for scaling 
   
   training_df <- df[train, ]
   holding_df  <- df[hold, ]
   
-  clim_means <- colMeans(training_df[ , clim_vars], na.rm = TRUE)
-  clim_sds   <- apply(training_df[ , clim_vars ], 2, FUN = sd, na.rm = TRUE)
+  c_means <- colMeans(training_df[ , covars], na.rm = TRUE)
+  c_sds   <- apply(training_df[ , covars ], 2, FUN = sd, na.rm = TRUE)
   
-  training_df[ , clim_vars] <- scale( training_df[ , clim_vars], center = TRUE, scale = TRUE)
-  holding_df[  , clim_vars] <- scale( holding_df[  , clim_vars], center = clim_means, scale = clim_sds )
+  training_df[ , covars] <- scale( training_df[ , covars], center = TRUE, scale = TRUE)
+  holding_df[  , covars] <- scale( holding_df[  , covars], center = c_means, scale = c_sds )
   
   training_df <-  training_df [ order(training_df$year), ]
   holding_df  <-  holding_df [ order(holding_df$year), ]
@@ -30,7 +32,7 @@ scale_clim_covs <- function(df , train, hold, clim_vars ){
 } 
 
 
-growth_dataframe2datalist <- function(df, train, hold, clim_vars){ 
+growth_dataframe2datalist <- function(df, train, hold){ 
   
   # Function simply makes list of data for STAN models  
   
@@ -38,7 +40,9 @@ growth_dataframe2datalist <- function(df, train, hold, clim_vars){
     
   # --------split into training and holding data and scale climate covariates -------------------------
   
-  out <- scale_clim_covs(df, train, hold, clim_vars)
+  out <- scale_covs(df, train, hold)
+  
+  covars <- grep( '^[PTW]\\.', names(out[[1]])) # grab all the covariates for scaling 
   
   training_df <- out[[1]]
   holding_df <- out[[2]]
@@ -50,7 +54,7 @@ growth_dataframe2datalist <- function(df, train, hold, clim_vars){
   Y         <- training_df$logarea.t1                             # plant size at time t + 1 
   X         <- training_df$logarea.t0                             # plant size at time t   
   
-  C         <- as.matrix(training_df[, clim_vars])                # all climate covariates 
+  C         <- as.matrix(training_df[, covars])                # all climate covariates 
   Covs      <- ncol(C)                                            # number of climate covariates 
   
   W         <- as.matrix(training_df[, grep('W', names(training_df)) ]) # crowding matrix 
@@ -67,8 +71,8 @@ growth_dataframe2datalist <- function(df, train, hold, clim_vars){
   npreds    <- nrow(holding_df)                                   # total predicted observations, modern data 
   y_holdout <- holding_df$logarea.t1                              # plant size at time t, modern data 
   Xhold     <- holding_df$logarea.t0                              # plant size at time t-1, modern data 
-  Chold     <- holding_df[ , clim_vars]                           # climate matrix, modern data 
-  Whold     <- holding_df[ , grep('W', names(holding_df)) ]       # crowding matrix, modern data 
+  Chold     <- holding_df[ , covars]                           # climate matrix, modern data 
+  #Whold     <- holding_df[ , grep('W', names(holding_df)) ]       # crowding matrix, modern data 
   gid_out   <- as.numeric(holding_df$Group)                       # group id, modern data 
   yid_out   <- as.numeric(as.factor(holding_df$year))             # year id, modern data
   nyrs_out  <- length(unique(holding_df$year))                    # num years, modern data 
@@ -88,7 +92,7 @@ growth_dataframe2datalist <- function(df, train, hold, clim_vars){
   N2        <- nrow(survival_df)                                   # total predictions
   X2        <- survival_df$logarea.t0                              # plant size at time t-1 
   C2        <- survival_df$C                                        # climate matrix 
-  W2        <- survival_df$W                                        # crowding matrix 
+  #W2        <- survival_df$W                                        # crowding matrix 
   gid2      <- as.numeric(survival_df$Group)                       # group id 
   yid2      <- as.numeric(as.factor(survival_df$year))             # year id
   nyrs2     <- length(unique(survival_df$year))                    # num years 
@@ -100,25 +104,27 @@ growth_dataframe2datalist <- function(df, train, hold, clim_vars){
   
   return( 
     list(
-      N = N, Y = Y, X = X , gid = gid, G = G, Yrs = nyrs, yid = yid , Covs = Covs, C = C, W = W, W_covs = W_covs,
-      gid_out = gid_out, npreds = npreds, y_holdout = y_holdout, Xhold = Xhold, Chold = Chold, Whold = Whold, yid_out = yid_out, nyrs_out = nyrs_out, treat_out = treat_out,
+      N = N, Y = Y, X = X , gid = gid, G = G, Yrs = nyrs, yid = yid , Covs = Covs, C = C,
+      gid_out = gid_out, npreds = npreds, y_holdout = y_holdout, Xhold = Xhold, Chold = Chold, yid_out = yid_out, nyrs_out = nyrs_out, treat_out = treat_out,
       trackid = trackid, trackid_out = trackid_out, year = year, year_out = year_out, quad = quad, quad_out = quad_out,        
       tau_beta = 1, 
       spp = spp, 
-      N2 = N2, X2 = X2, C2 = C2, W2 = W2, gid2 = gid2, yid2 = yid2, nrys2 = nyrs2, treat2 = treat2, trackid2 = trackid2, 
+      N2 = N2, X2 = X2, C2 = C2, gid2 = gid2, yid2 = yid2, nrys2 = nyrs2, treat2 = treat2, trackid2 = trackid2, 
       year2 = year2, quad2 = quad2 
     )
   )
 }
 
-survival_dataframe2datalist <- function(df, train, hold, clim_vars){
+survival_dataframe2datalist <- function(df, train, hold, covars){
 
   # Function simply makes list of data for STAN models
 
   # --------split into training and holding data and scale climate covariates -------------------------
 
-  out <- scale_clim_covs(df, train, hold, clim_vars)
-
+  out <- scale_covs(df, train, hold)
+  
+  covars <- grep( '^[PTW]\\.', names(out[[1]])) # grab all the covariates for scaling 
+  
   training_df <- out[[1]]
   holding_df <- out[[2]]
 
@@ -129,11 +135,11 @@ survival_dataframe2datalist <- function(df, train, hold, clim_vars){
   Y         <- training_df$survives                               # plant survival at time t + 1   
   X         <- training_df$logarea                                # plant size at time t
 
-  C         <- as.matrix(training_df[, clim_vars])                # all climate covariates
+  C         <- as.matrix(training_df[, covars])                # all climate covariates
   Covs      <- ncol(C)                                            # number of climate covariates
 
-  W         <- as.matrix(training_df[, grep('W', names(training_df)) ]) # crowding matrix
-  W_covs    <- ncol(W)                                            # number of species in crowding matrix
+   W         <- as.matrix(training_df[, grep('W', names(training_df)) ]) # crowding matrix
+  #W_covs    <- ncol(W)                                            # number of species in crowding matrix
 
   gid       <- as.numeric(training_df$Group)                      # integer id for each plot area
   G         <- length(unique(training_df$Group))                  # number of groups representing exclosure areas
@@ -150,8 +156,8 @@ survival_dataframe2datalist <- function(df, train, hold, clim_vars){
   npreds    <- nrow(holding_df)                                   # total predicted observations, modern data
   y_holdout <- holding_df$survives                                # plant survival at time t + 1, modern data
   Xhold     <- holding_df$logarea                                 # plant size at time t, modern data
-  Chold     <- holding_df[ , clim_vars]                           # climate matrix, modern data
-  Whold     <- holding_df[ , grep('W', names(holding_df)) ]       # crowding matrix, modern data
+  Chold     <- holding_df[ , covars]                           # climate matrix, modern data
+  #Whold     <- holding_df[ , grep('W', names(holding_df)) ]       # crowding matrix, modern data
   gid_out   <- as.numeric(holding_df$Group)                       # group id, modern data
   yid_out   <- as.numeric(as.factor(holding_df$year))             # year id, modern data
   nyrs_out  <- length(unique(holding_df$year))                    # num years, modern data
@@ -163,8 +169,8 @@ survival_dataframe2datalist <- function(df, train, hold, clim_vars){
   
   return(
     list(
-      N = N, Y = Y, X = X , gid = gid, G = G, Yrs = nyrs, yid = yid , Covs = Covs, C = C, W = W, W_covs = W_covs,
-      gid_out = gid_out, npreds = npreds, y_holdout = y_holdout, Xhold = Xhold, Chold = Chold, Whold = Whold, yid_out = yid_out, nyrs_out = nyrs_out, treat_out = treat_out,
+      N = N, Y = Y, X = X , gid = gid, G = G, Yrs = nyrs, yid = yid , Covs = Covs, C = C,
+      gid_out = gid_out, npreds = npreds, y_holdout = y_holdout, Xhold = Xhold, Chold = Chold, yid_out = yid_out, nyrs_out = nyrs_out, treat_out = treat_out,
       trackid = trackid, trackid_out = trackid_out, year = year, year_out = year_out, quad = quad, quad_out = quad_out,              
       tau_beta = 1, 
       spp = spp
@@ -174,13 +180,15 @@ survival_dataframe2datalist <- function(df, train, hold, clim_vars){
 
 
 
-recruitment_dataframe2datalist <- function(df, train, hold, clim_vars){
+recruitment_dataframe2datalist <- function(df, train, hold){
   
   # Function simply makes list of data for STAN models
   
   # --------split into training and holding data and scale climate covariates -------------------------
   
-  out <- scale_clim_covs(df, train, hold, clim_vars)
+  clim_vars <- names(df)[ grep('^[PT]\\.', names(df)) ] 
+  
+  out <- scale_covs(df, train, hold)
   
   training_df <- out[[1]]
   holding_df <- out[[2]]
@@ -198,15 +206,12 @@ recruitment_dataframe2datalist <- function(df, train, hold, clim_vars){
   parents1  <- as.matrix(training_df[, grep('^cov', names(training_df)) ])/100  # parents in plot 
   parents2  <- as.matrix(training_df[, grep('^Gcov', names(training_df))])/100  # parents in group
   
-  parents1   <- parents1[parents1 > 0 ]                             # eliminate zeros 0 so that values can be fit with neg_binom 
-  parents2  <- parents2[parents2 > 0 ]                             # eliminate zeros so that values can be fit with neg_binom 
+  parents1   <- parents1[ apply( parents1, 1, function( x ) all(x != 0 ) ), ]       # eliminate zeros 0 so that values can be fit with neg_binom 
+  parents2   <- parents2[ apply( parents2, 1, function( x ) all(x != 0 ) ), ]       # eliminate zeros 0 so that values can be fit with neg_binom 
   
   spp_list  <- factor( str_extract( colnames(parents1), '[A-Z]+$'))        # get species names 
-
   Nspp      <- length(spp_list)                                   # number of parent species
-  
   species_name   <- unique(training_df$species )
-  
   spp       <- as.numeric( spp_list [spp_list == species_name ] ) # assign species number to datalist 
    
   gid       <- as.numeric(training_df$Group)                      # integer id for each plot area
@@ -225,9 +230,10 @@ recruitment_dataframe2datalist <- function(df, train, hold, clim_vars){
   parents1_out  <- as.matrix(holding_df[, grep('^cov',  names(holding_df)) ])/100  # parents in plot 
   parents2_out  <- as.matrix(holding_df[, grep('^Gcov', names(holding_df))])/100  # parents in group
   
-  parents1_out   <- parents1_out[parents1_out > 0 ]                             # eliminate zeros 0 so that values can be fit with neg_binom 
-  parents2_out  <- parents2_out[parents2_out > 0 ]                             # eliminate zeros so that values can be fit with neg_binom 
+  parents1_out   <- parents1_out[ apply( parents1_out, 1, function( x ) all(x != 0 ) ), ]       # eliminate zeros 0 so that values can be fit with neg_binom 
+  parents2_out   <- parents2_out[ apply( parents2_out, 1, function( x ) all(x != 0 ) ), ]       # eliminate zeros 0 so that values can be fit with neg_binom 
   
+    
   gid_out   <- as.numeric(holding_df$Group)                       # group id, modern data
   yid_out   <- as.numeric(as.factor(holding_df$year))             # year id, modern data
   nyrs_out  <- length(unique(holding_df$year))                    # num years, modern data
@@ -263,10 +269,32 @@ make_stan_datalist <- function(vr, data_path, clim_vars, clim_file, ... ) {
   
   dlist <- lapply( dfiles, readRDS)
   
-  # -- subset ------------------------------------------------------------------------------#
+  # -- subset -------------------------------------------------------------------------------#
   all_data <- lapply(dlist, function(x){ subset(x, year > 1926 & !Treatment %in% c('No_shrub', 'No_grass'))} )
   
-  all_data <- lapply(all_data, merge, y = clim_covs, by = c('Treatment', 'Period', 'year')) 
+  all_data <- lapply(all_data, merge, y = clim_covs[, c('Treatment', 'Period', 'year', clim_vars)], by = c('Treatment', 'Period', 'year')) 
+  
+  # -- make size by interaction effects -------------------------------------------------------------# 
+  
+  if(vr == 'survival'){ 
+    all_data <- lapply( all_data, function(x) { names(x)[names(x) == 'logarea'] <- 'logarea.t0'; x } )
+  } 
+  
+  if( vr != 'recruitment') { 
+    all_data <- 
+      lapply( all_data, function( x ) {  
+      ifx <- x[, clim_vars]*x[, 'logarea.t0']   ##### climate by size interactions 
+      names(ifx ) <- paste0(clim_vars , ':', 'logarea.t0')
+      cbind(x, ifx)
+    })
+    
+    all_data <- 
+      lapply( all_data, function( x ) {  
+        ifx <- x[, grep('W.', names(x))]*x[, 'logarea.t0']   ##### competition by size interactions 
+        names(ifx ) <- paste0(names(ifx)[grep('W', names(ifx))] , ':', 'logarea.t0')
+        cbind(x, ifx)
+    })
+  }  
   
   # -- make training and holding subsets ----------------------------------------------------# 
   
@@ -276,7 +304,7 @@ make_stan_datalist <- function(vr, data_path, clim_vars, clim_file, ... ) {
   # -- prepare for stan ---------------------------------------------------------------------# 
   fxn_list <- c('growth_dataframe2datalist', 'survival_dataframe2datalist', 'recruitment_dataframe2datalist')
   
-  all_data_list <- mapply( FUN = match.fun( fxn_list [ grep(vr, fxn_list) ]), df = all_data, train = training, hold = holding, MoreArgs = list( 'clim_vars' = clim_vars), SIMPLIFY = FALSE)
+  all_data_list <- mapply( FUN = match.fun( fxn_list [ grep(vr, fxn_list) ]), df = all_data, train = training, hold = holding, SIMPLIFY = FALSE)
   
   names(all_data_list) <- spp_names
   
