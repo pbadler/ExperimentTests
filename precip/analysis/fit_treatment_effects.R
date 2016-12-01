af5b708bdf8eaef1d4d84f375b7059ce641c323a
Andy Kleinhesselink <andy.kleinhesselink@gmail.com>
2016-12-01 15:52:17 -0700

    changing the thinning on the fit treatment effects

--- precip/analysis/fit_treatment_effects.R
+++ precip/analysis/fit_treatment_effects.R
@@ -9,14 +9,14 @@ for(i in 1:nrow(df)){
   
   test <- readRDS(paste0('data/temp_data/', vr, '_data_lists_for_stan.RDS'))[[spp]]
   
-  myfit <- stan(paste0('analysis/', vr, '/model_', vr, '_treatment_effects.stan'), data = test, cores = 4, iter = 2000, thin = 2)
+  myfit <- stan(paste0('analysis/', vr, '/model_', vr, '_treatment_effects.stan'), data = test, cores = 4, iter = 2000, thin = 4)
   
   ss <-  get_sampler_params(myfit) 
   
   dv <- sum(   unlist( lapply( ss, function(x) sum( x[ (1 + ceiling(0.5*nrow(x))):nrow(x), 'divergent__']) )))
   
   if ( dv > 0 ){ 
-    myfit <- stan(paste0('analysis/', vr, '/model_', vr, '_treatment_effects.stan'), data = test, cores = 4, iter = 2000, thin = 2, 
+    myfit <- stan(paste0('analysis/', vr, '/model_', vr, '_treatment_effects.stan'), data = test, cores = 4, iter = 2000, thin = 4, 
                   control = list(adapt_delta = 0.95, stepsize = 0.4, max_treedepth = 20))
   } 
   
