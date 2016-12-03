6535d6d9c88b02884997fc1caf34d3b265140418
Andy Kleinhesselink <andy.kleinhesselink@gmail.com>
2016-12-03 14:23:02 -0700

    adding script to plot the predicted year and treatment effects as intercept and slope parameters

--- precip/analysis/figure_scripts/plot_predicted_year_effects.R
+++ precip/analysis/figure_scripts/plot_predicted_year_effects.R
@@ -56,7 +56,7 @@ get_parameter_estimates <- function( fit_file, treatment_fit_file, dat_files) {
   if (vr != 'recruitment') {
     X <- dat$Xhold
     YE2 <-
-      sweep(YE2, 2, X, '/') # divide TREATMENT EFFECT by size to isolate the climate effects
+      sweep(YE2, 2, X, '/') # divide individual TREATMENT X SIZE EFFECTS by size to isolate the slope parameter 
   }
   
   YE1_df <- data.frame( base_df,  parameter = 'a (intercept)', t(YE1) ) 
@@ -73,46 +73,63 @@ get_parameter_estimates <- function( fit_file, treatment_fit_file, dat_files) {
     
   avg_pred <- 
     pred_df %>% 
-    filter( year > 2010 & Group == 1) %>% 
-    group_by(species, vital_rate, year, Treatment, parameter, type , iteration ) %>% 
+    group_by(species, vital_rate, year, Treatment, parameter, type, iteration ) %>% 
     summarise( val = mean(val)) 
 
   # get observed treatment effects ---------------------------------------- # 
-  tm <- rstan::extract(readRDS(treatment_fit_file), 'tm')$
+
   bt <- rstan::extract(readRDS(treatment_fit_file), 'bt')$bt 
   a  <- rstan::extract(readRDS(treatment_fit_file), 'a')$a
   
-  df1 <- data.frame( base_df, parameter = 'a (intercept)',  t(a) )
+  df1 <- data.frame( unique( base_df[ , c('species', 'vital_rate', 'year')]), parameter = 'a (intercept)',  Treatment = 'Control' , t(a) )
 
-  
   if(vr != 'recruitment' ) {
     b1 <- rstan::extract(readRDS(treatment_fit_file), 'b1')$b1
-    df2 <- data.frame( base_df, parameter = 'b1 (size)', t(b1))
+    df2 <- data.frame( unique( base_df[ , c('species', 'vital_rate', 'year')]), parameter = 'b1 (size)', Treatment = 'Control', t(b1))
     
+    year_df <- rbind(df1, df2)
+    
+    year_df <- 
+      year_df %>% 
+      gather(iteration, val, starts_with('X')) %>%
+      spread(Treatment, val)
+     
     treatment_pars <-  
       data.frame( species = spp, 
                   vital_rate = vr, 
-                  type = 'observed_effect',
-                  type2 = 'treatment_parameter', 
                   parameter = c('a (intercept)','a (intercept)', 'b1 (size)', 'b1 (size)') ,  
                   Treatment = c('Drought', 'Irrigation', 'Drought', 'Irrigation'), 
                   t(bt))
     
-    
-  
+
   }else{ 
-    obs_effect <-  
+    year_df <- df1 
+    
+    year_df <- 
+      year_df %>% 
+      gather(iteration, val, starts_with('X')) %>%
+      spread(Treatment, val)
+    
+    treatment_pars <-  
       data.frame( species = spp, 
                   vital_rate = vr, 
-                  type = 'observed_effect',
                   parameter = c('a (intercept)','a (intercept)'),  
                   Treatment = c('Drought', 'Irrigation'), 
                   t(bt) )
   }
   
-  obs_effect <-  
+  treatment_pars <- 
+    treatment_pars %>% 
+    gather( iteration, val, starts_with('X')) %>%
+    spread( Treatment, val )
+  
+  obs_effect <- merge( year_df , treatment_pars, by = c('species', 'vital_rate', 'parameter', 'iteration'))
+  
+  obs_effect <- 
     obs_effect %>% 
-    gather( iteration, val , starts_with('X'))
+    mutate( type = 'observed_effect') %>%
+    mutate( Drought = Drought + Control , Irrigation = Irrigation + Control ) %>%
+    gather( Treatment, val, Control:Irrigation)
   
   all_effects <- bind_rows( avg_pred, obs_effect)
   
@@ -127,20 +144,20 @@ treatment_stan_fit  <- dir( 'output/stan_fits', 'treatment_fit', full.names = T)
 
 all_effects <- list()
 
+
 for( i in 1:length(fit_files)) {   
-  
+    
   all_effects[[i]] <- get_parameter_estimates( fit_files[i], treatment_stan_fit[i], dat_files )
 
 }
-
-all_effects[[2]]
 
 all_effects <- do.call(rbind, all_effects)
 
 par_plot_df <- 
   all_effects %>% 
-  filter( !(vital_rate == 'recruitment' & parameter == 'b1 (size)')) %>% 
-  group_by( species, vital_rate, Treatment, type, parameter ) %>% 
+  filter( !(vital_rate == 'recruitment' & parameter == 'b1 (size)')) %>%
+  filter( !(Treatment != 'Control' & year < 2011 )) %>% 
+  group_by( species, vital_rate, year, Treatment, type, parameter ) %>% 
   summarise( center = mean(val), lbci = quantile( val , 0.025), ubci = quantile(val, 0.975))
 
 par_plot <- 
@@ -149,16 +166,15 @@ par_plot <-
   geom_point(position = position_dodge(width = 0.5), size = 3) + 
   geom_errorbar(position = position_dodge(width = 0.5), width = 0.2) + 
   geom_hline( aes( yintercept = 0 ), linetype = 2 , alpha = 0.5) + 
-  facet_grid( parameter ~ .  ) + 
+  facet_grid( year  ~ parameter   ) + 
   ylab ( 'Mean effect (+/- 95% Bayesian Credible Interval)') + 
   theme_bw()
   
 gg <- par_plot_df %>% group_by( species, vital_rate ) %>% do( gg = par_plot %+% . + ggtitle(paste( 'Parameter estimates for', .$species, .$vital_rate, sep = ' ')))
 
-pdf( 'figures/observed_vs_predicted_treatment_parameters.pdf', width = 8, height = 5)
+pdf( 'figures/observed_vs_predicted_year_parameters.pdf', width = 8, height = 5)
   gg$gg
 dev.off()
-
 
 compare_plot_df <- 
   par_plot_df %>% 
@@ -166,7 +182,7 @@ compare_plot_df <-
   filter( stat == 'center')%>%
   spread(type, val)  
 
-pdf( 'figures/compare_predicted_vs_observed_treatment_parameters.pdf', width = 8, height = 5)
+pdf( 'figures/compare_predicted_vs_observed_year_parameters.pdf', width = 8, height = 5)
   
 ggplot( compare_plot_df, aes( x = predicted_effect, y = observed_effect))  + 
   geom_point() + 
@@ -181,6 +197,11 @@ ggplot( compare_plot_df, aes( x = predicted_effect, y = observed_effect))  +
 ggplot( compare_plot_df, aes( x = predicted_effect, y = observed_effect))  + 
   geom_point() + 
   facet_wrap( ~ Treatment ) + 
+  geom_smooth(aes(group = 1), se = F, method = 'lm')
+
+ggplot( compare_plot_df, aes( x = predicted_effect, y = observed_effect))  + 
+  geom_point() + 
+  facet_grid( Treatment ~ year ) + 
   geom_smooth(aes(group = 1), se = F, method = 'lm')
 
 dev.off()
