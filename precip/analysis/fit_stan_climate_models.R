84c9683196d246beb31856cfde80155f27f27d2c
Andy Kleinhesselink <andy.kleinhesselink@gmail.com>
2016-12-09 10:09:02 -0700

    making minor change to the climate model fitting scrip to account for divergent transitions

--- precip/analysis/fit_stan_climate_models.R
+++ precip/analysis/fit_stan_climate_models.R
@@ -5,7 +5,7 @@ library(stringr)
 dl_files <- dir('data/temp_data', 'modified_.*_data_lists_for_stan.RDS', full.names = T)
 m_files  <- dir('analysis', 'model.*_1.stan', recursive = T, full.names = T)
 
-for( i in 1:length(dl_files)){ 
+for( i in 3:length(dl_files)){ 
   
   dl  <- readRDS(dl_files[i])
   vr  <- str_extract(dl_files[i], c('growth', 'recruitment', 'survival'))
@@ -15,15 +15,33 @@ for( i in 1:length(dl_files)){
   for( j in 1:length(dl)){ 
     
     if(is.null( ncol(dl[[j]]$C)) ){ 
-      fit <- stan(str_replace(m_files[i], '.stan', '_special.stan'), data  = dl[[j]], thin = 4, cores = 4, iter = 2000, seed = 1)
+      myfit <- stan(str_replace(m_files[i], '.stan', '_special.stan'), data  = dl[[j]], thin = 4, cores = 4, iter = 2000, seed = 1)
     }else{
-      fit <- stan(m_files[i], data  = dl[[j]], thin = 4, cores = 4, iter = 2000, seed = 1)
+      myfit <- stan(m_files[i], data  = dl[[j]], thin = 4, cores = 4, iter = 2000, seed = 1)
     }
     
-    saveRDS(fit, paste0('output/stan_fits/', spp[j], '_', vr, '_climate_fit.RDS'))
+    ss <-  get_sampler_params(myfit) 
+    
+    dv <- sum(   unlist( lapply( ss, function(x) sum( x[ (1 + ceiling(0.5*nrow(x))):nrow(x), 'divergent__']) )))
+    
+    if ( dv > 0 ) { 
+      # try again if divergence 
+      myfit <- stan( fit = myfit, data = dl[[j]], cores = 4, iter = 4000, thin = 8, seed = 1)
+    }
+    
+    ss <-  get_sampler_params(myfit) 
+    
+    dv <- sum(   unlist( lapply( ss, function(x) sum( x[ (1 + ceiling(0.5*nrow(x))):nrow(x), 'divergent__']) )))
+    
+    if ( dv > 0 ){ 
+      # try again if divergent 
+      myfit <- stan( fit = myfit, data = dl[[j]], cores = 4, iter = 2000, thin = 4, 
+                     control = list(adapt_delta = 0.85, stepsize = 0.8, max_treedepth = 20), seed = 1 )
+    } 
+    
+    saveRDS(myfit, paste0('output/stan_fits/', spp[j], '_', vr, '_climate_fit.RDS'))
     
   }
-  
-  rm( fit )
+  rm( myfit )
 }
 
