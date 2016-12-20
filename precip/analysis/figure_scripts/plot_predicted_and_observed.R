ac6ac3d516d70042c2b9656af602eb1c925a4358
Andy Kleinhesselink <andy.kleinhesselink@gmail.com>
2016-12-19 23:52:54 -0700

    adding a script to calculate MSE of all predictions

--- precip/analysis/figure_scripts/plot_predicted_and_observed.R
+++ precip/analysis/figure_scripts/plot_predicted_and_observed.R
@@ -8,14 +8,13 @@ library(rstan)
 # functions ---------------------------------------------------------------------------- 
 
 #
+load('analysis/figure_scripts/my_plotting_theme.Rdata')
 
 setwd('~/Documents/ExperimentTests/precip/')
 
-my_colors <- c('#1b9e77', '#d95f02', '#7570b3')
-
 years <- expand.grid(year = 1925:2017, Treatment = c(1:3), stat = c('true_cov', 'pred_cover'))
-years$Period[ years$year > 2006 ] <- 'Modern'
-years$Period[ years$year <= 1960 ] <- 'Historical'
+years$Period[ years$year > 2010 ] <- 'Modern'
+years$Period[ years$year <= 2010 ] <- 'Historical'
 
 # output lists 
 survives <- list(NA)
@@ -24,7 +23,6 @@ recruits <- list(NA)
 
 species_list <- c('ARTR', 'HECO', 'POSE', 'PSSP')
 
-ylims <- list( c(0,40), c(0,7.5), c(0,7.5), c(0,7.5))
 i = 1
 
 for( i in 1:4) {  
@@ -37,77 +35,47 @@ for( i in 1:4) {
   m <- dir('output/stan_fits', paste0( spp, '.*_climate_fit.RDS'), full.names = TRUE)
   m1 <- dir('output/stan_fits', paste0( spp, '.*year_effects_fit.RDS'), full.names = TRUE)
   
-  smu <- summary(readRDS(m[3]), c('muhat'))$summary[, 1]
-  plot( sdl$Xhold , smu ) 
+  sdf <- data.frame(sdl[ c('treathold', 'Yhold', 'gidhold', 'yearhold')] )
+  rdf <- data.frame(rdl[ c('treathold', 'Yhold', 'gidhold', 'yearhold')] )
+  gdf <- data.frame(gdl[ c('treathold', 'Yhold', 'gidhold', 'yearhold')] )
   
-  fit <- readRDS(m[1])
-  fity <- readRDS(m1[1])
+  sdf$climate <- summary(readRDS(m[3]), c('muhat'))$summary[, 1]
+  rdf$climate <- summary(readRDS(m[2]), c('lambda_pred'))$summary[, 1]
+  gdf$climate <- summary(readRDS(m[1]), c('muhat'))$summary[, 1]
   
-  gmu <- summary(fit, c('mu'))$summary[,1]
-  df1 <- data.frame(obs = gdl$obs_id, Y = gdl$Y, mu = gmu, type = '1')
-  plot(df1$mu, df1$Y) 
-  abline( 0, 1, col = 'red')
+  sdf$year_effects <- summary(readRDS(m1[3]), c('muhat'))$summary[, 1]
+  rdf$year_effects <- summary(readRDS(m1[2]), c('lambda_pred'))$summary[, 1]
+  gdf$year_effects <- summary(readRDS(m1[1]), c('muhat'))$summary[, 1]
   
-  gmu <- summary(fit, c('muhat'))$summary[,1]
-  df2 <- data.frame(obs = gdl$obs_idhold, Y = gdl$Yhold, mu = gmu, 'type' = 'hold')
-  plot( df1$mu, df1$Y , col = 'gray')
-  points(df2$mu, df2$Y) 
-  abline( 0, 1, col = 'red')
+  sdf$vital_rate <- 'survival'
+  gdf$vital_rate <- 'growth'
+  rdf$vital_rate <- 'recruitment'
   
-  df <- rbind( df1, df2)
+  df <- rbind( sdf, gdf, rdf )
   
-  gmu <- summary(fit, c('muhat3'))$summary[,1]
-  df3 <- data.frame(obs = gdl$obs_id3, mu3 = gmu)
+  names(df) <- c('Treatment', 'Yhold', 'Group', 'year', 'climate', 'year_effects', 'vital_rate')
+  df$Treatment <- factor(df$Treatment, labels = c('Control', 'Drought', 'Irrigation'))
   
-  df <- df %>% 
-    spread(type, mu) 
+  df <- 
+    df %>% 
+    gather( model, mu , climate:year_effects )
   
-  df <- merge( df, df3)
+  df$SE <- (df$Yhold - df$mu)^2  
+  df$species <- spp 
   
-  plot(df$mu3, df$Y) 
-  abline( 0, 1, col = 'red')
-  points(df$hold, df$Y, col = 'blue')
+  overall_MSE <- 
+    df %>% 
+    group_by( species, vital_rate, model ) %>% 
+    summarise( MSE = mean(SE))
   
-  gmu <- summary( fity, 'muhat')$summary[,1]
-  df2y <- data.frame( obs = gdl$obs_idhold, Y = gdl$Yhold, mu = gmu, type  = 'hold')
-  plot( df2y$mu, df2y$Y)
-  points(df$hold, df$Y, col = 'gray')
-  abline(0,1)
+  treatment_MSE <-   
+    df %>% 
+    group_by( species, vital_rate, Treatment, model ) %>% 
+    summarise( MSE = mean(SE))
   
-  test <- read.csv('data/temp_data/ARTR_growth_and_survival_cleaned_dataframe.csv')
-  
-  plot(test$logarea.t0, test$logarea.t1, col = 'gray')
-  points(data = subset(test, Period == 'Modern'), logarea.t1 ~ logarea.t0)
-  m1 <- lm(data = subset(test, Period == 'Historical'), logarea.t1 ~ logarea.t0)
-  abline(m1)
-  m2 <- lm(data = subset(test, Period == 'Modern'), logarea.t1 ~ logarea.t0)
-  abline(m2, col = 'blue')
-  abline(0,1, lty = 2)
-  
-  
-  
-  gmu <- summary(readRDS(m[1]), c('muhat'))$summary[,1]
-  
-  pdf( paste0( 'figures/predictions/', spp  , '_predicted_cover.pdf' ), height = 8, width = 8) 
-  
-  print( 
-    ggplot( subset( plot_cover[[i]], Period == "Historical"), aes( x = year, y =  val, color = Treatment, linetype = stat)) +
-      geom_line() +
-      scale_color_manual(values = my_colors ) + 
-      ylim( ylims[[i]]) + 
-      scale_x_continuous()
-  )
-  
-  print( 
-    ggplot( subset( plot_cover[[i]], Period == "Modern"), aes( x = year, y =  val, color = Treatment, linetype = stat)) +
-      geom_line() +
-      scale_color_manual(values = my_colors ) + 
-      ylim( ylims[[i]] ) + 
-      scale_x_continuous()
-  )
-  
-  dev.off()
+  out[[i]] <- overall_MSE
 } 
 
-saveRDS(size , 'output/predicted_size.RDS')
-saveRDS(survives, 'output/predicted_survival.RDS')
+out <- do.call( rbind, out )
+
+write.csv(out, 'output/all_MSE_scores.csv', row.names = F)
