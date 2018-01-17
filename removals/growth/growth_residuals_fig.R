e64257949fe86e59346cef1102008f711598a789
peterbadler <peter.adler@usu.edu>
2018-01-16 22:01:38 -0700

    Finished first versoin of growth residuals figure

--- removals/growth/growth_residuals_fig.R
+++ removals/growth/growth_residuals_fig.R
@@ -27,7 +27,8 @@ for(i in 1:4){
 }
 
 # plot figure
-par(mfrow=c(2,2),tcl=-0.2,mgp=c(2,0.5,0),mar=c(3,3,2,1),oma=c(2,2,0,0))
+png("growth_residuals_figure.png",height=5.5,width=8,units="in",res=400)
+par(mfrow=c(2,2),tcl=-0.2,mgp=c(2,0.5,0),mar=c(2,2,2,1),oma=c(2,2,0,0))
 for(i in 1:4){
   if(i==1){
     keep <- which(growth_residuals[[i]]$Treatment=="No_grass")
@@ -37,6 +38,21 @@ for(i in 1:4){
   plot(growth_residuals[[i]]$cover[keep],growth_residuals[[i]]$resids[keep],xlab="",ylab="")
   abline(h=0,lty="dashed")
   mtext(side=3,spp_names[i],line=0.5,adj=0,font=3)
+  
+  # fit simple linear regression, display results if significant
+  tmp <- lm(growth_residuals[[i]]$resids[keep]~growth_residuals[[i]]$cover[keep])
+  if(anova(tmp)$Pr[1] < 0.05){
+    abline(tmp,col="black")
+    legend("topright",paste("P =", round(anova(tmp)$Pr[1],3)),bty="n")
+  }
+  
+  # # plot quadrat means on top
+  # quad_means <- aggregate(growth_residuals[[i]]$resids[keep],by=list(quad=growth_residuals[[i]]$quad[keep]),FUN="mean")
+  # quad_means <- merge(quad_means,unique(growth_residuals[[i]][,c("quad","cover")],arr.index=2),all.x=T)
+  # points(quad_means$cover,quad_means$x,pch=16,cex=1.2,col="red3")
 }
-mtext(side=1, "Pre-treatment cover of removal species",outer=T,cex=1.1)
-mtext(side=2, "Growth residuals",outer=T,cex=1.1)
+
+mtext(side=1, "Pre-treatment cover of removal species (%)",outer=T,line=0.5,cex=1.1)
+mtext(side=2, "Growth residuals",outer=T,line=0.5, cex=1.1)
+
+dev.off()
