9cf7c8920396380a418e6a5fa6504dd3a96f97cf
ellner <spe2@cornell.edu>
2018-03-27 12:24:10 -0400

    catchup

---
+++ removals/growth/CheckGrowthResiduals.R
@@ -0,0 +1,13 @@
+﻿resids = (allD$logarea.t1-m1$summary.fitted.values[,1])
+preds = m1$summary.fitted.values[,1]
+
+dev.new(); par(mfrow=c(2,2)); 
+eC = which(allD$Treatment=="Control");
+eR = which(allD$Treatment=="No_shrub");
+plot(preds[eC],resids[eC],xlab="Predicted", ylab="Observed-Predicted",ylim=range(resids)); title(main="POSE");  
+points(preds[eR],resids[eR],col="red") 
+
+scatter.smooth(preds[eC],resids[eC],main="Control"); abline(0,0,col="red",lty=2); 
+scatter.smooth(preds[eR],resids[eR],main="Removal"); abline(0,0,col="red",lty=2); 
+scatter.smooth(preds[eR],resids[eR],main="Removal",ylim=c(-1,1)); abline(0,0,col="red",lty=2); 
+abline(h=0.2,col="blue",lty=2); 
---
+++ removals/growth/Implied_alphaShrub.R
@@ -0,0 +1,55 @@
+﻿rm(list=ls(all=TRUE))
+graphics.off();
+
+root=ifelse(.Platform$OS.type=="windows","c:/Repos","~/repos"); # modify as needed
+setwd(paste(root,"/ExperimentTests/removals/growth",sep="")); # modify as needed 
+
+doSpp <- "POSE"
+sppList <- c("ARTR","HECO","POSE","PSSP","allcov","allpts")
+dataDir1 <- paste(root,"/ExperimentTests/data/idaho",sep="")
+dataDir2 <- paste(root,"/ExperimentTests/data/idaho_modern",sep="")
+nonCompLength.s=5 #Number of columns in SppData that are not measures of competitors 
+
+# set up distance weights------------------------------------------------
+dists <- read.csv(paste0(root,"/ExperimentTests/data/idaho/speciesdata/IdahoDistanceWeights.csv"))
+#dists <- read.csv(paste(dataDir2,"/speciesdata/IdahoModDistanceWeights_noExptl.csv",sep=""));
+dists$allcov <- rowMeans(dists[,1:4])  # for "other" polygons use average of big 4
+dists$allpts <- dists$POSE  # set forb dist wts = smallest grass (POSE)
+
+# import old data--------------------------------------------------------
+source("fetchGrowthData.r")
+
+D1 <- fetchGdat(doSpp=doSpp,speciesList=sppList,datadir=dataDir1,distWts=dists)
+D1$Treatment <- "Control"
+
+# import modern data--------------------------------------------------------
+D2 <- fetchGdat(doSpp=doSpp,speciesList=sppList,datadir=dataDir2,distWts=dists)
+
+# merge in treatment data
+tmp <- read.csv(paste(dataDir2,"/quad_info.csv",sep=""))
+tmp <- tmp[,c("quad","Treatment")]
+D2 <- merge(D2,tmp, all.x=T)
+
+# account for removal in baseline years
+if(doSpp!="ARTR"){
+  ii <- which(D2$year>=2011 & D2$Treatment=="No_shrub")
+  D2$W.ARTR[ii] <- 0
+}else{
+  ii <- which(D2$year>=2011 & D2$Treatment=="No_grass")
+   D2$W.HECO[ii] <- 0 ; D2$W.POSE[ii] <- 0 ; D2$W.PSSP[ii] <- 0
+}
+
+# combine old and modern
+allD <- rbind(D1,D2); rm(D1,D2,tmp)
+
+# merge data on removals at individual level
+tmp <- read.csv(paste(dataDir2,"/speciesData/",doSpp,"/",doSpp,"_within_ARTRremovals.csv",sep=""))
+tmp <- tmp[,c("quad","year","trackID","inARTR")] 
+allD<-merge(allD,tmp,all.x=T)
+allD$inARTR[is.na(allD$inARTR)] <- 0
+
+W.ARTR = mean(allD$W.ARTR[allD$Treatment=="Control"]); 
+impliedAlphaPOSE = 0.2/W.ARTR; impliedAlphaPOSE; 
+
+W.POSE = mean(allD$W.POSE[allD$Treatment=="Control"]); 
+0.61*W.POSE 
