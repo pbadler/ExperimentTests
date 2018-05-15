be5fb30b74462287dc76a4d33fa024eecd49ead6
Andy Kleinhesselink <andy.kleinhesselink@gmail.com>
2018-05-15 16:03:34 -0700

    Edit analysis pipeline add comments about run time
    Edit prepare datalists to run with internal climate data

--- precip/analysis/analysis_pipeline.R
+++ precip/analysis/analysis_pipeline.R
@@ -1,3 +1,10 @@
+############################################################# 
+#
+# NOTE: The whole thing takes several hours to run. (most of day?)
+# Involves three rounds of model fitting. 
+#
+#############################################################
+
 rm(list = ls() ) 
 library(rstan)
 library(dplyr)
@@ -27,52 +34,53 @@ source('analysis/treatment_trends_precip.R')
 
 # 3. fit treatment models 
 
-source('analysis/fit_stan_treatment_models.R')
+source('analysis/fit_stan_treatment_models.R') # takes a few hours to run 
 
-source('analysis/figure_scripts/plot_treatment_stan_fits.R')
+source('analysis/figure_scripts/plot_treatment_stan_fits.R') # takes a few minutes to run 
 
-source('analysis/summarize_treatment_model_fits.R')
+source('analysis/summarize_treatment_model_fits.R') # take several minutes
 
 source('analysis/figure_scripts/plot_treatment_parameter_estimates.R')
 
 # 4. run year effects model 
   
- source('analysis/fit_stan_year_effects_models.R')
- source('analysis/summarize_year_model_fits.R')
- source('analysis/figure_scripts/plot_year_model_fits.R')
+source('analysis/fit_stan_year_effects_models.R')
+
+source('analysis/summarize_year_model_fits.R')
+source('analysis/figure_scripts/plot_year_model_fits.R')
 
 # 5. find climate correlations 
 
- source('analysis/find_climate_year_effects_correlations.R')
+source('analysis/find_climate_year_effects_correlations.R')
 
- source('analysis/select_climate_covariates.R')
+source('analysis/select_climate_covariates.R')
 
 
 # 6. modify datalists with selected climate covariates
 
- source('analysis/modify_climate_datalists.R')
+source('analysis/modify_climate_datalists.R')
 
 # 7. fit climate models 
 
- source('analysis/fit_stan_climate_models.R')
+source('analysis/fit_stan_climate_models.R')
 
- source('analysis/check_for_divergent_transitions.R')
+source('analysis/check_for_divergent_transitions.R')
 
- source('analysis/figure_scripts/plot_climate_fits.R')
+source('analysis/figure_scripts/plot_climate_fits.R')
 
- source('analysis/figure_scripts/plot_compare_year_model_climate_model_year_effects.R')
+source('analysis/figure_scripts/plot_compare_year_model_climate_model_year_effects.R')
 
- source('analysis/summarize_climate_model_fits.R')
+source('analysis/summarize_climate_model_fits.R')
 
- source('analysis/figure_scripts/plot_climate_parameter_estimates.R')
+source('analysis/figure_scripts/plot_climate_parameter_estimates.R')
 
 # 8. get prediction scores 
 
-  source('analysis/calculate_lppd.R')
-  source('analysis/calculate_total_lppd.R')
-  source('analysis/get_MSE_scores.R')
+source('analysis/calculate_lppd.R')
+source('analysis/calculate_total_lppd.R')
+source('analysis/get_MSE_scores.R')
 
-  source('analysis/make_prediction_score_tables.R')
+source('analysis/make_prediction_score_tables.R')
 
 # 9. plot observed and predicted climate effects on vital rates 
 
