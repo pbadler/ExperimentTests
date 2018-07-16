4786a94a4f4daf84eef6a01df17b9e4bbf035013
peterbadler <peter.adler@usu.edu>
2018-07-16 17:27:21 -0600

    Rewrote enet growth script

--- precip/analysis/fit_enet_growth_year_effects.R
+++ precip/analysis/fit_enet_growth_year_effects.R
@@ -62,145 +62,142 @@ for(i in 1:length(sppList)){
   
 }
 
-### 2. elastic net on INTERCEPT ------------------------------------------------------------
+### 2. fit elastic net on intercept and slope ------------------------------------------------------------
 
+enet_predictions <- vector("list",length(sppList))
 best_coefs <- matrix(NA,length(sppList),ncol(Cdat)-4) # warning: hardwired "4" on climate columns
 colnames(best_coefs) <- names(Cdat[,5:ncol(Cdat)])
-rmse_ratio <- numeric(length(sppList))
-
-pdf("figures/enet_growth_yr_effects.pdf",height=4,width=4)
-par(tcl=-0.2,mgp=c(2,0.5,0),mar=c(3,4,2,1))
+best_coefs_slope <- best_coefs
 
 for(i in 1:length(sppList)){
   
   doSpp <- sppList[i]
   
-  ## model Intercept year effects
-  # prepare data
+  # prepare training data
   trainD <- yrBetas[[i]] %>% filter(year < 2011, year > 1926) # drop first year because of NAs in covariates
-  y <- trainD$Intercept
+  y_int <- trainD$Intercept
+  y_slope <- trainD$logarea.t0
   X <- trainD[,7:NCOL(trainD)]
   X_mean <- colMeans(X); X_sd <- apply(X,MARGIN=2,FUN="sd")
   X <- scale(X, center = X_mean, scale = X_sd)
+  
+  # prepare out of sample data
+  newD <- yrBetas[[i]] %>% filter(year >= 2011)
+  y_new_int <- newD$Intercept
+  y_new_slope <- newD$logarea.t0
+  X_new <- newD[,7:NCOL(newD)]
+  X_new <- scale(X_new, center = X_mean, scale = X_sd)
+  
+  # fit elastic net on intercept
   pen_facts <- rep(1, ncol(X)) # penalize all covariates
   lambdas <- 10^seq(2, -2, by = -0.005) # sequence of penalties to test
-  
-  
-  enet_out <- cv.glmnet(x = X, 
-                       y = y, 
+  enet_int <- cv.glmnet(x = X, 
+                       y = y_int, 
                        lambda = lambdas,
                        penalty.factor = pen_facts,
                        family = "gaussian", 
                        alpha = 0.5, # 0 for ridge, 1 for lasso 
                        standardize = FALSE, 
                        type.measure = "mse",
-                       nfolds = 12) #length(y))
+                       nfolds = length(y_int))
   
-  # look at results
-  # plot(log(enet_out$lambda),enet_out$cvm,xlab="log(Lambda)",ylab="CV score",type="l")
-  
-  #matplot(log(enet_out$lambda),t(enet_out$glmnet.fit$beta),type="l")
-  #abline(v=log(enet_out$lambda.min),lty="dashed",col="darkgrey")
-  
-  best_coefs[i,] <- enet_out$glmnet.fit$beta[,which(enet_out$lambda==enet_out$lambda.min)]
+  best_coefs[i,] <- enet_int$glmnet.fit$beta[,which(enet_int$lambda==enet_int$lambda.min)]
   
   # predictions for training data
-  y_hat <- predict(enet_out,newx=X,s="lambda.min")
+  y_hat_int <- predict(enet_int,newx=X,s="lambda.min")
   
-  # get out of sample MSE
-  newD <- yrBetas[[i]] %>% filter(year >= 2011)
-  y_new <- newD$Intercept
-  X_new <- newD[,7:NCOL(newD)]
-  X_new <- scale(X_new, center = X_mean, scale = X_sd)
-  y_hat_new <- predict(enet_out,newx=X_new, s="lambda.min")
-  mse_new <- mean((y_new-y_hat_new)^2)
-  
-  # make figure
-  plot(c(y,y_new),c(y_hat,y_hat_new),type="n",xlab="Observed",ylab="Predicted",
-       ylim=c(min(c(y,y_new,y_hat,y_hat_new)),max(c(y,y_new,y_hat,y_hat_new))),
-       xlim=c(min(c(y,y_new,y_hat,y_hat_new)),max(c(y,y_new,y_hat,y_hat_new))),
-       main=paste0(sppList[i]," growth year effects (Intercept)"))
-  abline(0,1)
-  points(y,y_hat)
-  points(y_new[which(newD$Treatment=="Control")],y_hat_new[which(newD$Treatment=="Control")],pch=16)
-  points(y_new[which(newD$Treatment=="Drought")],y_hat_new[which(newD$Treatment=="Drought")],pch=16,col="red")
-  points(y_new[which(newD$Treatment=="Irrigation")],y_hat_new[which(newD$Treatment=="Irrigation")],pch=16,col="blue")
-  legend("topleft",c("Control (training)","Control (out-of-sample)","Drought (out-of-sample)",
-                     "Irrigation (out-of-sample)"),pch=c(1,16,16,16),
-                      col=c("black","black","red","blue"),bty="n",cex=0.8)
-}
+  # predictions for out of sample data
+  y_hat_new_int <- predict(enet_int,newx=X_new, s="lambda.min")
 
-saveRDS(best_coefs,"analysis/growth/enet_best_coefs_intercept.RDS")
-
-
-### 3. elastic net on SLOPE year effects ------------------------------------------------------------
-
-best_coefs_slope <- matrix(NA,length(sppList),ncol(Cdat)-4) # warning: hardwired "4" on climate columns
-colnames(best_coefs_slope) <- names(Cdat[,5:ncol(Cdat)])
-rmse_ratio <- numeric(length(sppList))
-
-for(i in 1:length(sppList)){
-  
-  doSpp <- sppList[i]
-  
-  ## model Intercept year effects
-  # prepare data
-  trainD <- yrBetas[[i]] %>% filter(year < 2011, year > 1926) # drop first year because of NAs in covariates
-  y <- trainD$logarea.t0
-  X <- trainD[,7:NCOL(trainD)]
-  X_mean <- colMeans(X); X_sd <- apply(X,MARGIN=2,FUN="sd")
-  X <- scale(X, center = X_mean, scale = X_sd)
-  pen_facts <- rep(1, ncol(X)) # penalize all covariates
-  lambdas <- 10^seq(2, -2, by = -0.005) # sequence of penalties to test
-  
-  
-  enet_out <- cv.glmnet(x = X, 
-                       y = y, 
+  # fit elastic net on slope year effects
+  enet_slope <- cv.glmnet(x = X, 
+                       y = y_slope, 
                        lambda = lambdas,
                        penalty.factor = pen_facts,
                        family = "gaussian", 
                        alpha = 0.5, # 0 for ridge, 1 for lasso 
                        standardize = FALSE, 
                        type.measure = "mse",
-                       nfolds = 12) # length(y))
+                       nfolds = length(y_slope))
   
-  # look at results
-  # plot(log(enet_out$lambda),enet_out$cvm,xlab="log(Lambda)",ylab="CV score",type="l")
-  
-  #matplot(log(enet_out$lambda),t(enet_out$glmnet.fit$beta),type="l")
-  #abline(v=log(enet_out$lambda.min),lty="dashed",col="darkgrey")
-  
-  best_coefs_slope[i,] <- enet_out$glmnet.fit$beta[,which(enet_out$lambda==enet_out$lambda.min)]
+  best_coefs_slope[i,] <- enet_slope$glmnet.fit$beta[,which(enet_slope$lambda==enet_slope$lambda.min)]
   
   # predictions for training data
-  y_hat <- predict(enet_out,newx=X,s="lambda.min")
+  y_hat_slope <- predict(enet_slope,newx=X,s="lambda.min")
   
-  # get out of sample MSE
-  newD <- yrBetas[[i]] %>% filter(year >= 2011)
-  y_new <- newD$logarea.t0
-  X_new <- newD[,7:NCOL(newD)]
-  X_new <- scale(X_new, center = X_mean, scale = X_sd)
-  y_hat_new <- predict(enet_out,newx=X_new, s="lambda.min")
-  mse_new <- mean((y_new-y_hat_new)^2)
+  # predictions for out of sample data
+  y_hat_new_slope <- predict(enet_slope,newx=X_new, s="lambda.min")
   
-  # make figure
+  # save output
+  enet_predictions[[i]] <- list(trts = newD$Treatment,
+                                y_int=y_int,y_hat_int=y_hat_int,y_new_int=y_new_int, y_hat_new_int=y_hat_new_int,
+                                y_slope=y_slope, y_hat_slope=y_hat_slope, y_new_slope=y_new_slope, y_hat_new_slope=y_hat_new_slope)
+  saveRDS(best_coefs,"analysis/growth/enet_best_coefs_intercept.RDS")
+  saveRDS(best_coefs_slope,"analysis/growth/enet_best_coefs_slope.RDS")
+
+}
+
+# clean up
+rm(list= ls()[!(ls() %in% c('best_coefs','best_coefs_slope','enet_predictions','sppList','yrBetas','size_info'))]) 
+
+
+### 3. Draw figures -------------------------------------------  
+
+# custom plotting function
+obs_pred_fig <- function(my_dat, vital_rate, effect="int", legend_location){
+  # my_dat is a list containing observations and predictions
+  # vital_rate is a string: either "growth" or "survival"
+  # effect is a string with default "int" and alternative value "slope"
+  # legend_location is a string, such as "topleft"
+  
+  # subset data of interest
+  trts <- my_dat$trts
+  keep <- grep(effect,names(my_dat))
+  my_dat <- my_dat[keep]
+  
+  # trim names
+  names(my_dat) <- sub(paste0("_",effect),"",names(my_dat))
+  
+  attach(my_dat)
   plot(c(y,y_new),c(y_hat,y_hat_new),type="n",xlab="Observed",ylab="Predicted",
        ylim=c(min(c(y,y_new,y_hat,y_hat_new)),max(c(y,y_new,y_hat,y_hat_new))),
        xlim=c(min(c(y,y_new,y_hat,y_hat_new)),max(c(y,y_new,y_hat,y_hat_new))),
-       main=paste0(sppList[i]," growth year effects (Slope)"))
+       main=paste(sppList[i],vital_rate,effect, "year effects"))
   abline(0,1)
   points(y,y_hat)
-  points(y_new[which(newD$Treatment=="Control")],y_hat_new[which(newD$Treatment=="Control")],pch=16)
-  points(y_new[which(newD$Treatment=="Drought")],y_hat_new[which(newD$Treatment=="Drought")],pch=16,col="red")
-  points(y_new[which(newD$Treatment=="Irrigation")],y_hat_new[which(newD$Treatment=="Irrigation")],pch=16,col="blue")
-  legend("topleft",c("Control (training)","Control (out-of-sample)","Drought (out-of-sample)",
+  points(y_new[which(trts =="Control")],y_hat_new[which(trts =="Control")],pch=16)
+  points(y_new[which(trts =="Drought")],y_hat_new[which(trts =="Drought")],pch=16,col="red")
+  points(y_new[which(trts =="Irrigation")],y_hat_new[which(trts =="Irrigation")],pch=16,col="blue")
+  legend(legend_location,c("Control (training)","Control (out-of-sample)","Drought (out-of-sample)",
                      "Irrigation (out-of-sample)"),pch=c(1,16,16,16),
                       col=c("black","black","red","blue"),bty="n",cex=0.8)
+  detach(my_dat)
 }
 
-saveRDS(best_coefs_slope,"analysis/growth/enet_best_coefs_slope.RDS")
+# loop through species and plot observed and predicted intercepts and slopes
+
+pdf("figures/enet_growth_yr_effects.pdf",height=4,width=8)
+par(mfrow=c(1,2),tcl=-0.2,mgp=c(2,0.5,0),mar=c(3,4,2,1))
+
+for(i in 1:length(sppList)){
+  
+  obs_pred_fig(enet_predictions[[i]], vital_rate="growth", effect="int", legend_location= "topleft")
+  obs_pred_fig(enet_predictions[[i]], vital_rate="growth", effect="slope", legend_location= "topleft")
+  
+}
 
 dev.off()
+
+
+
+
+
+
+
+
+
+
+
 
 
 
